3ee3639a0c5077d09660e925ba16ee2d16e10b19
Solvery <solvery@163.com>
2015-08-17 09:51:31 +0800

    rename

--- javascript/class.2.3.js
+++ javascript/class.2.3.js
@@ -6,8 +6,8 @@ function Cat(name,color){
 	　　　　this.type = "猫科动物";
 	　　　　this.eat = function(){alert("吃老鼠");};
 	　　}
-	　　var cat1 = new Cat("大毛","黄色");
-	　　var cat2 = new Cat ("二毛","黑色");
-	　　alert(cat1.type); // 猫科动物
-	　　cat1.eat(); // 吃老鼠
-	alert(cat1.eat == cat2.eat); //false
+var cat1 = new Cat("大毛","黄色");
+var cat2 = new Cat ("二毛","黑色");
+alert(cat1.type); // 猫科动物
+cat1.eat(); // 吃老鼠
+alert(cat1.eat == cat2.eat); //false
--- javascript/class4.prototype.js
+++ javascript/class4.prototype.js
@@ -1,27 +1,27 @@
 alert=console.log
 
 function Cat(name,color){
-	    this.name = name;
-	    this.color = color;
-	  }
-	  Cat.prototype.type = "猫科动物";
-	  Cat.prototype.eat = function(){alert("吃老鼠")};
+	this.name = name;
+	this.color = color;
+}
+Cat.prototype.type = "猫科动物";
+Cat.prototype.eat = function(){alert("吃老鼠")};
 
 var cat1 = new Cat("大毛","黄色");
-  var cat2 = new Cat("二毛","黑色");
-  alert(cat1.type); // 猫科动物
-  cat1.eat(); // 吃老鼠
+var cat2 = new Cat("二毛","黑色");
+alert(cat1.type); // 猫科动物
+cat1.eat(); // 吃老鼠
 
 alert(cat1.eat == cat2.eat); //true
 
 alert(Cat.prototype.isPrototypeOf(cat1)); //true
-　　alert(Cat.prototype.isPrototypeOf(cat2)); //true
+alert(Cat.prototype.isPrototypeOf(cat2)); //true
 
 alert(cat1.hasOwnProperty("name")); // true
-　　alert(cat1.hasOwnProperty("type")); // false
+alert(cat1.hasOwnProperty("type")); // false
 
 alert("name" in cat1); // true
-　　alert("type" in cat1); // true
+alert("type" in cat1); // true
 
 for(var prop in cat1) { alert("cat1["+prop+"]="+cat1[prop]); }
 

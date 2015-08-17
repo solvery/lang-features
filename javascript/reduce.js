ad992b922f6031964d7194ad0438c6d88bc5d603
Solvery <solvery@163.com>
2015-08-17 09:25:38 +0800

    add reduce.js

---
+++ javascript/reduce.js
@@ -0,0 +1,17 @@
+
+function reduce(actionFunction, list, initial){
+	var accumulate;
+	var temp;
+	if(initial){
+		accumulate = initial;
+	}
+	else{
+		accumulate = list.shfit();
+	}
+	temp = list.shift();
+	while(temp){
+		accumulate = actionFunction(accumulate,temp);
+		temp = list.shift();
+	}
+	return accumulate;
+};

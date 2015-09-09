4d2fb6724c467b191dd25667b8a1bd78e6b86e18
Solvery <solvery@163.com>
2015-09-09 17:39:24 +0800

    erlang

--- erlang/function_1.erl
+++ erlang/function_1.erl
@@ -16,10 +16,14 @@ start() ->
 
     io:format("~f~n", [area_1({circle, 10})]),
     io:format("~f~n", [area_2({circle, 10})]),
+
     io:format("~s~n", [sign(2)]),
+    io:format("~s~n", [sign(-2)]),
+
     %io:format("~s~n", []),
     io:format("~n").
 
+% pattern match
 either_or_both_1(true, _)		-> true;
 either_or_both_1(_, true)		-> true;
 either_or_both_1(false, false)	-> false;
@@ -29,6 +33,7 @@ either_or_both_2(true, B) when is_boolean(B)	-> true;
 either_or_both_2(A, true) when is_boolean(A)	-> true;
 either_or_both_2(false, false)					-> false.
 
+% case 
 either_or_both_3(A,B) -> 
 	case {A,B} of 
 		{_,true}	-> true;
@@ -36,7 +41,6 @@ either_or_both_3(A,B) ->
 		{flase,flash} -> flase
 	end.
 
-%
 area_1({circle, Radius})	-> Radius * Radius * math:pi();
 area_1({square, Side})	-> Side * Side;
 area_1({rectangle, Height, Width})	-> Height * Width.
@@ -51,7 +55,7 @@ area_2(Shape) ->
             Height * Width
     end.
 
-%
+% if 
 sign(N) when is_number(N) ->
     if
        N > 0 -> positive;
@@ -60,9 +64,4 @@ sign(N) when is_number(N) ->
     end.
 
 
-yesno(F) ->
-    case F(true, false) of
-        true  -> io:format("yes~n");
-        false -> io:format("no~n")
-    end.
 
---
+++ erlang/function_2.erl
@@ -0,0 +1,5 @@
+-module(function_2).
+-export([start/0]).
+
+start() ->
+    io:format("~n").

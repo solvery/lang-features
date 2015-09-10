d0655d9992f8cac96145dfa3fbbea6fc3c8b6612
Solvery <solvery@163.com>
2015-09-10 11:28:48 +0800

    erlang

--- erlang/list_2.erl
+++ erlang/list_2.erl
@@ -5,7 +5,12 @@
 
 start() ->
 	L1 = [1,2,3,4],
-    io:format("~p~n", [2*X || X <- L1]),
+    io:format("~p~n", [[X*2 || X <- L1]]),
+
+	L2 = [{a,1},{b,2},{c,3}],
+    io:format("~p~n", [ [{C,2*N} || {C,N} <- L2 ] ]),
+
     io:format("~p~n", [0]),
+
     io:format("~n").
 

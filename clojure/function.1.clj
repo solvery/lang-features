607e3f617c4fe3b50045a7ca4fd891d8a97b7a07
Solvery <solvery@163.com>
2015-09-15 15:38:16 +0800

    clojure

--- clojure/function.1.clj
+++ clojure/function.1.clj
@@ -20,3 +20,27 @@
 
 ; lambda 
 (fn [x] (* x x))
+
+;; fib
+(defn fib [n] 
+    (if (= n 0) 0
+        (if (= n 1) 1
+            (+ (fib (- n 1)) (fib (- n 2))))))
+
+(defn lazy-seq-fibo 
+    ([] 
+        (concat [0 1] (lazy-seq-fibo 0 1))) 
+    ([a b] 
+        (let [n (+ a b)] 
+            (lazy-seq 
+                (cons n (lazy-seq-fibo b n))))))
+
+; closure
+(println (reduce + 
+    (filter even? 
+        (take-while (fn [n] (< n 4000000)) (lazy-seq-fibo)))))
+
+; 使用 # 创建闭包
+(println (reduce + 
+    (filter even? 
+        (take-while #(< % 4000000) (lazy-seq-fibo)))))

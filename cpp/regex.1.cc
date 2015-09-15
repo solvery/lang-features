2ed41c7e20362c4e26de7bfaa2901a28cccd1b9e
Solvery <solvery@163.com>
2015-09-15 17:28:40 +0800

    cpp

--- cpp/regex.1.cc
+++ cpp/regex.1.cc
@@ -6,7 +6,9 @@ int main(int argc, char** arg) {
 	{
 		regex rx(".*ll.*");
 		bool match = regex_match("hello", rx);
-		regex rx("lorem", icase);
+	}
+	{
+		regex rx("lorem", regex_constants::icase);
 		bool match = regex_match("Lorem", rx);
 	}
 

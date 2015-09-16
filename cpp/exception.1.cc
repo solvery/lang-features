4b0cafcb19d74224454656b796159509c8e92fb2
Solvery <solvery@163.com>
2015-09-16 09:23:06 +0800

    cpp

--- cpp/exception.1.cc
+++ cpp/exception.1.cc
@@ -6,9 +6,56 @@
 
 using namespace std;
 
+void risky() {
+	if (1 < 10) {
+		throw runtime_error("bam!");
+	}
+}
+
+class Bam : public runtime_error {
+	public:
+		Bam() : runtime_error("bam!") {}
+};
+
+
 int main(int argc, char** arg) {
 	{
-		exception
+		try {
+			risky();
+		}
+		catch (const exception &e) {
+			cout << e.what() << endl;
+		}
+	}
+	{
+		try {
+			risky();
+		}
+		catch (const exception& e) {
+			cout << "an error occurred..." << endl;
+			throw;
+		}
+	}
+	{
+		//throw Bam();
+	}
+	{
+		try {
+			risky();
+		}
+		catch (const system_error &e) {
+			cout << "system error: " << e.code()
+				<< endl;
+		}
+		catch (const exception &e) {
+			cout << "exception: " << e.what()
+				<< endl;
+		}
+		catch (...) {
+			cout << "unknown error" << endl;
+		}
+	}
+	{
 	}
 }
 

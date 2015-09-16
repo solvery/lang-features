335c10e2305c9f7fe8fd683cac5ee8264909b36a
Solvery <solvery@163.com>
2015-09-16 09:14:02 +0800

    cpp

---
+++ cpp/exception.1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <system_error>
+#include <typeinfo>
+
+using namespace std;
+
+int main(int argc, char** arg) {
+	{
+		exception
+	}
+}
+
+

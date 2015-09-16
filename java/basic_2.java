b68679cbf71d0ea30cef71b432610dba6b0b9318
Solvery <solvery@163.com>
2015-09-16 14:31:19 +0800

    java

---
+++ java/basic_2.java
@@ -0,0 +1,47 @@
+
+public class basic_2 {
+    public static void main(String[] args) {
+		{
+			int i;
+			int j = 3;
+		}
+		{
+			final int i = 7;
+			int n;
+			n = 3;
+		}
+		{
+			int n = 1;
+			int one = n++;
+			int three = ++n;
+			int two = --n;
+		}
+		{
+			Integer i = new Integer(0);
+		}
+		{
+			String s2 = "hello";
+			String s1 = s2 == null ? "was null" : s2;
+		}
+		{
+			Math.pow(2.0, 32.0);
+			Math.sqrt(2);
+			(long)3.77;
+			Math.round(3.77);
+			(long)Math.floor(3.77);
+			(long)Math.ceil(3.77);
+			Math.abs(-7);
+			Math.abs(-7.77);
+		}
+		{
+		}
+		{
+		}
+		{
+		}
+		{
+		}
+	}
+}
+
+

897cfaff83d0858d04626bb9781fa0602a3e8e9e
Solvery <solvery@163.com>
2015-09-18 15:00:20 +0800

    java

---
+++ java/package_1.java
@@ -0,0 +1,15 @@
+
+// import package_a.package_a;
+
+public class package_1 {
+
+    public static void main(String[] args) {
+		package_a.package_a o1 = new package_a.package_a();
+		o1.m1();
+
+		package_a.p1.p2.p3.package_b o2 = new package_a.p1.p2.p3.package_b();
+		o2.m1();
+    }
+}
+
+
---
+++ java/package_a/p1/p2/p3/package_b.java
@@ -0,0 +1,13 @@
+
+package package_a.p1.p2.p3; 
+
+public class package_b {
+	public void m1() {
+		System.out.println("hello package b.");
+	}
+    public static void main(String[] args) {
+    }
+}
+
+
+

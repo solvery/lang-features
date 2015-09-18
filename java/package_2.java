ed9909bb03e0a6ba39b18299d34962e1a2c891bb
Solvery <solvery@163.com>
2015-09-18 14:48:51 +0800

    java

---
+++ java/package_2.java
@@ -0,0 +1,12 @@
+
+import package_a.package_a;
+
+public class package_1 {
+
+    public static void main(String[] args) {
+		package_1 o1 = new package_1();
+		o1.m1();
+    }
+}
+
+
---
+++ java/package_a/package_a.java
@@ -0,0 +1,12 @@
+
+package package_a;
+
+public class package_a {
+	public void m1() {
+		System.out.println("hello package a.");
+	}
+    public static void main(String[] args) {
+    }
+}
+
+

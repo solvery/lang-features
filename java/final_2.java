7d612d86fd75e308084d99effd3a32f05eadc5f5
Solvery <solvery@163.com>
2015-09-18 11:40:15 +0800

    java

--- java/final_2.java
+++ java/final_2.java
@@ -1,18 +1,21 @@
 // final method, cannot override
 
 class Base {
-	public final void m1() {
-	}
+	public final void m1() { }
+
+	private final void m2() { }
+
 	// overload
-	public void m1(int a) {
-	}
+	public void m1(int a) { }
 }
 
-public class final_2 extends {
+public class final_2 extends Base {
 
 	// override
-	public void m1() {
-	}
+	public void m1() { }
+
+	// ok
+	public void m2() { }
 
     public static void main(String[] args) {
     }

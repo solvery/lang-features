258aa34c1a11713f18a590353ea435dd8e01b877
Solvery <solvery@163.com>
2015-09-22 14:54:08 +0800

    java

---
+++ java/strictfp_1.java
@@ -0,0 +1,13 @@
+
+strictfp interface I {
+	/// strictfp void m1();
+}
+
+public strictfp class strictfp_1 {
+	public strictfp void m1() {
+	}
+    public static void main(String[] args) {
+    }
+}
+
+

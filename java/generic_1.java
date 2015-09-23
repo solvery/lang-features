6f0b013d78a1e87d3e08bc765a8aef4326efde35
Solvery <solvery@163.com>
2015-09-23 10:08:42 +0800

    java

--- java/generic_1.java
+++ java/generic_1.java
@@ -8,7 +8,10 @@ class Foo<A> {
 
 public class generic_1 {
 	public static void main(String[] args) {
-		Foo<String> f = new Foo<String>("foo");
+		Foo<String> o1 = new Foo<String>("foo");
+		System.out.println(o1.a);
+		Foo<Integer> o2 = new Foo<Integer>(2);
+		System.out.println(o2.a);
 	}
 }
 

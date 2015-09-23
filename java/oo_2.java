11864848b17b7494b67bf444348ed04e4bbae1eb
Solvery <solvery@163.com>
2015-09-23 14:26:13 +0800

    java

--- java/oo_2.java
+++ java/oo_2.java
@@ -1,6 +1,15 @@
+// static class
 
 public class oo_2 {
+	static class A {
+		private static int v1 = 5;
+		private int v2 = 2;
+	}
     public static void main(String[] args) {
+		System.out.println(A.v1);
+		/// System.out.println(A.v2);
+		System.out.println(new A().v1);
+		System.out.println(new A().v2);
     }
 }
 

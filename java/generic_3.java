3ba0a2fe8dfee0debfce9abe9039776c0c8f7a9d
Solvery <solvery@163.com>
2015-09-23 14:15:43 +0800

    java

--- java/generic_3.java
+++ java/generic_3.java
@@ -1,18 +1,19 @@
 
-class Foo<T extends String> {
+class Foo<T super String> {
 	public T a;
 	public Foo(T a) {
 		this.a = a;
 	}
 }
 
-public class generic_2 {
+public class generic_3 {
 	public static void main(String[] args) {
 		Foo<String> o1 = new Foo<String>("foo");
 		System.out.println(o1.a);
 		/// Foo<Integer> o2 = new Foo<Integer>(2);
 	}
 }
+
 
 
 

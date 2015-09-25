24775cef7775658e8e47766bfc769fe7a3e2657c
Solvery <solvery@163.com>
2015-09-25 15:16:20 +0800

    java

--- java/oo_reflection_4.java
+++ java/oo_reflection_4.java
@@ -1,20 +1,28 @@
 
 class A{
-	public int v;
-	public A(){}  /// must have default constructor.
+	public int v1;
+	public int v2;
+	public A(){}
 	public A(int a){
-		this.v = a;
+		this.v1 = a;
+	}
+	public A(int a, int b){
+		this.v1 = a;
+		this.v2 = b;
 	}
 }
 
-public class oo_reflection_3 {
+public class oo_reflection_4 {
     public static void main(String[] args) throws Exception {
 		Class<?> c1 = null;
 		c1=Class.forName("A");
 
 		A o1 =  null;
+		A o2 =  null;
+		A o3 =  null;
 		o1 = (A)c1.newInstance();
     }
 }
+
 
 

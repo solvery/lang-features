0fe9ffe4c1afa75c6a230134682b62dc9ab4a726
Solvery <solvery@163.com>
2015-09-25 18:28:02 +0800

    java

---
+++ java/generic_5.java
@@ -0,0 +1,13 @@
+// <T> T
+
+public class generic_5 {
+    public static void main(String[] args) {
+		m1();
+    }
+	public static <T> T m1(){
+		T v1 = null;
+		return v1;
+	}
+	public <T> void m2(){}
+}
+

8c95c15f9452b358a834631a12b31c3c57dd2492
Solvery <solvery@163.com>
2015-09-25 09:50:12 +0800

    java

--- java/hash_1.java
+++ java/hash_1.java
@@ -5,8 +5,9 @@ public class hash_1 {
     public static void main(String[] args) {
 		{
 			// TreeMap<String, Integer> m = new TreeMap<String, Integer>();
-			// TreeMap<String, Integer> m = new HashMap<String, Integer>();
-			// TreeMap<String, Integer> m = new Hashtable<String, Integer>();
+			// Map<String, Integer> m = new TreeMap<String, Integer>();
+			// Map<String, Integer> m = new HashMap<String, Integer>();
+			// Map<String, Integer> m = new Hashtable<String, Integer>();
 			Map<String, Integer> m = new Hashtable<String, Integer>();
 			m.put("foo", 2);
 			m.put("hello", 5);
@@ -18,12 +19,6 @@ public class hash_1 {
 			Iterator<String> it = m.keySet().iterator();
 			String first = it.next();
 			System.out.println(first);
-
-			Iterator<Integer> it2 = m.entrySet().iterator();
-			while (m.hasNext()) {
-				Map.Entry<String, Integer> e = it2.next();
-				System.out.println(e);
-			}
 
 			for ( Map.Entry<String, Integer> e : m.entrySet() ) {
 				  e.getKey(); 

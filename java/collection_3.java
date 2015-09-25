ec528dba264ca444740c1c3e43e8b28d77f42275
Solvery <solvery@163.com>
2015-09-25 11:14:34 +0800

    java

--- java/collection_3.java
+++ java/collection_3.java
@@ -16,9 +16,18 @@ public class collection_3 {
     public static void main(String[] args) {
 		List<A> c1 = Arrays.asList(
 				new A(1),
-				new A(2),
 				new A(3),
+				new A(2),
 				new A(5));
+		System.out.println(c1);
+
+		Collections.sort(c1, new Comparator<A>() {
+			@Override
+			public int compare(A p1, A p2) {
+				return p1.v - p2.v;
+			}
+		});
 		System.out.println(c1);
 	}
 }
+

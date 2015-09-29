8a1e5be6c82ef6dc27dae4a0e8e8b2a4cb31e331
Solvery <solvery@163.com>
2015-09-29 09:51:33 +0800

    c#

--- c#/string_1.cs
+++ c#/string_1.cs
@@ -20,13 +20,20 @@ namespace CsharpExample {
 			System.Console.WriteLine(double.Parse("65") );
 			System.Console.WriteLine(decimal.Parse("65"));
 			System.Console.WriteLine();
-			System.Console.WriteLine(System.String.Join(", ", "hello"));
+			string[] names = "Bob Ned Amy".Split(' ');
+			System.Console.WriteLine(System.String.Join(", ", names));
+			System.Console.WriteLine();
+			System.Console.WriteLine("hello".IndexOf("ll"));
+			System.Console.WriteLine("hello".Substring(2, 2));
 			System.Console.WriteLine();
 			System.Console.WriteLine();
-			System.Console.WriteLine();
-			System.Console.WriteLine();
-			System.Console.WriteLine();
-			System.Console.WriteLine();
+
+			char Char = 'A';
+			System.Char.IsLetter(Char);
+			System.Char.IsNumber(Char);
+			System.Char.IsWhiteSpace(Char);
+			System.Char.IsUpper(Char);
+			System.Char.IsLower(Char);
 		}
 	}
 }

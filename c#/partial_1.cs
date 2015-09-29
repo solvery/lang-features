74aa0c773efaca0eddd6d09ff417613457a64170
Solvery <solvery@163.com>
2015-09-29 18:10:30 +0800

    c#

---
+++ c#/partial_1.cs
@@ -0,0 +1,19 @@
+
+// File: A1.cs
+partial class A
+{
+	public int v1 = 1;
+}
+
+// File: A2.cs
+partial class A
+{
+	/// public int v1 = 2;
+	public int v2 = 2;
+}
+
+class Hello {
+	static void Main() {
+		System.Console.WriteLine();
+	}
+}

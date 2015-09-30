3a1cec20af9ed44a36d2f2b1550e2857057562c4
Solvery <solvery@163.com>
2015-09-30 14:06:01 +0800

    cpp

--- cpp/oo.friend.2.cc
+++ cpp/oo.friend.2.cc
@@ -2,24 +2,15 @@
 #include <iostream>
 
 class A {
-	public:
-		int a1;
-	protected:
-		int a2;
-	private:
-		int a3;
-	public:
-		friend B;
+	friend class B;
 };
 
 class B {
-
 };
 
 using namespace std;
 
 int main(int argc, char** arg) {
 	B ob;
-	ob.a1 = 1;
 }
 

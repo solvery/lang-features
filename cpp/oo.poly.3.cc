c4e2eff6f8b9e86bb7dd61683aae8935b05ee9b4
Solvery <solvery@163.com>
2015-10-01 10:36:27 +0800

    cpp

--- cpp/oo.poly.3.cc
+++ cpp/oo.poly.3.cc
@@ -3,30 +3,37 @@
 
 using namespace std;
 
-class A
-{
+class A {
 	public:
-		virtual void func()
-		{
-			cout << "A func"<< endl;
+		virtual void foo() {
+			cout << "A foo"<< endl;
+		}
+		void bar(A *pa) {
+			pa->foo();
 		}
 };
 
-class B : public  A
-{
+class B : public  A {
 	public:
-		void func()
-		{
-			cout << "B func" << endl;
+		void foo() {
+			cout << "B foo" << endl;
 		}
 };
+
 int main(int argc, char** arg) {
 
-	A* a ;
-	A aa;
-	B bb;
-	//a= &aa;
-	a=&bb;
-	a->func();
+	A* pa ;
+	B* pb ;
+	A oa;
+	B ob;
+	pa=&oa;
+	pa->foo();
+	pa=&ob;
+	pa->foo();
+
+	pa=&oa;
+	pa->bar(pa);
+	pa=&ob;
+	pa->bar(pa);
 }
 

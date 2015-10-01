dee1e06df9bf6ee744ded28553a9afecec3314e1
Solvery <solvery@163.com>
2015-10-01 12:56:42 +0800

    cpp

--- cpp/sizeof.2.cc
+++ cpp/sizeof.2.cc
@@ -4,17 +4,32 @@
 #define FORMAT(T, DESC) "%-8s\t%-6lu\t%s\n"
 #define PRINT_SIZE_OF(T, DESC) printf(FORMAT(T, DESC), #T, (unsigned long)sizeof(T), DESC)
 
-class A
-{
-};
+class A1 { };
+class A2 { char c; };
+class A3 { public: void foo(){} };
+class A4 { public: virtual void foo(){} };
+class A5 { public: virtual void foo()=0;};
+class B1 : public A1 { };
+class B2 : public A1 { char c; };
+class B3 : virtual public A1 { };
+class B4 : virtual public A2 { };
 
-class B : virtual public A
-{
-	int a;
-};
 int main(int argc, char** arg) {
 
-	PRINT_SIZE_OF(A, "");
-	PRINT_SIZE_OF(B, "");
+    printf("/%-8s\t/%-6s\t/%s\n\n", "type", "bytes", "desc");
+
+	PRINT_SIZE_OF(int*, "");
+	PRINT_SIZE_OF(long*, "");
+	PRINT_SIZE_OF(char*, "");
+	PRINT_SIZE_OF(float*, "");
+	PRINT_SIZE_OF(A1, "class A1 { };                             ");
+	PRINT_SIZE_OF(A2, "class A2 { char c; };                     ");
+	PRINT_SIZE_OF(A3, "class A3 { public: void foo(){} };        ");
+	PRINT_SIZE_OF(A4, "class A4 { public: virtual void foo(){} };");
+	PRINT_SIZE_OF(A5, "class A5 { public: virtual void foo()=0;};");
+	PRINT_SIZE_OF(B1, "class B1 : public A1 { };                 ");
+	PRINT_SIZE_OF(B2, "class B2 : public A1 { char c; };         ");
+	PRINT_SIZE_OF(B3, "class B3 : virtual public A1 { };         ");
+	PRINT_SIZE_OF(B4, "class B4 : virtual public A2 { };         ");
 }
 

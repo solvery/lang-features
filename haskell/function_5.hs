b022d8c00da20a25b7e15a601444ed9752c3a5dc
Solvery <solvery@163.com>
2015-10-04 19:47:31 +0800

    haskell

--- haskell/function_5.hs
+++ haskell/function_5.hs
@@ -9,8 +9,19 @@ sayMe 4 = "Four!"
 sayMe 5 = "Five!"   
 sayMe x = "Not between 1 and 5" 
 
+sayMe2 :: (Integral a) => a -> String
+sayMe2 dig
+    | dig == 1 = "one"
+    | dig == 2 = "two"
+    | dig == 3 = "three"
+    | otherwise = "tired."
+
 main = do
     print $ sayMe 1
     print $ sayMe 3
     print $ sayMe 7
+
+    print $ sayMe2 1
+    print $ sayMe2 3
+    print $ sayMe2 7
 

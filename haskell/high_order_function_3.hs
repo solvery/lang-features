e90000c25866170197b6679cc19d9a78951c2dd3
Solvery <solvery@163.com>
2015-10-04 21:23:23 +0800

    haskell

--- haskell/high_order_function_3.hs
+++ haskell/high_order_function_3.hs
@@ -2,5 +2,9 @@
 sum' :: (Num a) => [a] -> a   
 sum' xs = foldl (\acc x -> acc + x) 0 xs
 
+sum'' :: (Num a) => [a] -> a   
+sum'' = foldl (+) 0
+
 main = do
     print $ sum' [1,2,3,4]
+    print $ sum'' [1,2,3,4]

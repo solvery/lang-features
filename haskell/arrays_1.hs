bd903136940f3295e74252adb33d9a9984455f49
Solvery <solvery@163.com>
2015-10-04 16:59:22 +0800

    haskell

--- haskell/arrays_1.hs
+++ haskell/arrays_1.hs
@@ -12,3 +12,6 @@ main = do
 
     let a2 = array (0,4) [(i,i+1) | i <- [0..4]]
     putStrLn $ "dcl: " ++ show a2
+    
+    let a3 = array((0,0), (1,2)) [((i,j), i+j) | i <- [0..1], j <- [0..2]]
+    putStrLn $ "2d:  " ++ show a3
--- haskell/basic_2.hs
+++ haskell/basic_2.hs
@@ -1,3 +1,4 @@
+-- :: type
 
 s :: String
 s = "constant"

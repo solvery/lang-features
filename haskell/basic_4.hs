f4f1871fb034a1b86e788313717c03faf94b5fa4
Solvery <solvery@163.com>
2015-10-04 13:26:28 +0800

    haskell

--- haskell/basic_4.hs
+++ haskell/basic_4.hs
@@ -1,16 +1,25 @@
+import Data.Time
+import Data.Time.Calendar.WeekDate
+
 main = do
-    if 7 `mod` 2 == 0
-        then putStrLn "7 is even"
-        else putStrLn "7 is odd"
+    let i = 2
+    putStr $ "write " ++ show i ++ " as "
+    case i of
+        1 -> putStrLn "one"
+        2 -> putStrLn "two"
+        3 -> putStrLn "three"
 
-    if 8 `mod` 4 == 0
-        then putStrLn "8 is divisible by 4"
-        else return ()
-
-    let num = 9
+    now <- getCurrentTime
+    let (_, _, week) = toWeekDate . utctDay $ now
     putStrLn $
-        if num < 0
-            then show num ++ " is negative"
-            else if num < 10
-                then show num ++ " has 1 digit"
-                else show num ++ " has multiple digits"
+        case week of
+            6 -> "it's the weekend"
+            7 -> "it's the weekend"
+            _ -> "it's a weekday"
+
+    localtime <- utcToLocalZonedTime now
+    let hour = todHour . localTimeOfDay . zonedTimeToLocalTime $ localtime
+    case hour of
+        _
+            | hour < 12 -> putStrLn "it's before noon"
+            | otherwise -> putStrLn "it's after noon"

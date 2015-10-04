f324ec7b84622d39a327e02ae8c933c7035cf1ae
Solvery <solvery@163.com>
2015-10-04 13:24:52 +0800

    haskell

--- haskell/basic_3.hs
+++ haskell/basic_3.hs
@@ -1,6 +1,16 @@
-s :: String
-s = "constant"
-
 main = do
-    putStrLn s
+    if 7 `mod` 2 == 0
+        then putStrLn "7 is even"
+        else putStrLn "7 is odd"
 
+    if 8 `mod` 4 == 0
+        then putStrLn "8 is divisible by 4"
+        else return ()
+
+    let num = 9
+    putStrLn $
+        if num < 0
+            then show num ++ " is negative"
+            else if num < 10
+                then show num ++ " has 1 digit"
+                else show num ++ " has multiple digits"

c3cb4f9602e144539016dc4f810b8a54043556f6
Solvery <solvery@163.com>
2015-10-05 12:13:06 +0800

    haskell

--- haskell/type_1.hs
+++ haskell/type_1.hs
@@ -1,4 +1,30 @@
+
+-- data
 data Bool = False | True
+-- data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun 
+data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving (Show, Eq, Ord, Enum)
+
+tomorrow Sun = Mon
+tomorrow d = succ d
+
+yesterday Mon = Sun
+yesterday d = pred d
+
+-- type
+type Name = String
+type Author = String
+type ISBN = String
+type Price = Float
+-- first Book, type constructor
+-- second Book, data constructor
+-- data Book = Book Name Author ISBN Price deriving (Show, Eq)
+data Book = Book {
+    name    :: Name,
+    author  :: Author,
+    isbn    :: ISBN,
+    price   :: Price
+}
 
 main = do
-    print ""
+    print Mon
+    print [Mon .. Sun]

7a4481e20828266a7d761ca250240f8508fe0e3e
Solvery <solvery@163.com>
2015-10-05 13:09:19 +0800

    haskell

--- haskell/basic_2.hs
+++ haskell/basic_2.hs
@@ -39,3 +39,9 @@ main = do
     print c2
     print $ sin c2
 
+-- type parsing
+    print (read "1.234" :: Double)
+    print (read "123"   :: Int)
+    print (read "0x1c8" :: Int)
+    print (read "wat"   :: Int)
+

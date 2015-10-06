0a41f4ad934a1fc8b43a2dcd54e29b901f52401d
Solvery <solvery@163.com>
2015-10-06 23:48:05 +0800

    cobol

---
+++ cobol/basic_1.cob
@@ -0,0 +1,7 @@
+       Identification Division.
+        Program-ID. basic_1.
+       Environment Division.
+       Data Division.
+       Procedure Division.
+        Display "Hello World!" end-display
+       goback.

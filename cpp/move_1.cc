0b12f118b40b65eb9abbac4ca30f48b72b0471cf
Solvery <solvery@163.com>
2015-10-08 10:02:32 +0800

    cpp

---
+++ cpp/move_1.cc
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Movable
+{
+    Movable (Movable&&); //move constructor
+    Movable&& operator=(Movable&&); //move assignment operator
+};
+
+int main(int argc, char** arg) {
+}
+

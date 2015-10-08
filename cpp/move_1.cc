78a7d492876df1438fff966750b94767eeedda33
Solvery <solvery@163.com>
2015-10-08 14:39:35 +0800

    cpp

--- cpp/move_1.cc
+++ cpp/move_1.cc
@@ -9,19 +9,6 @@
 
 using namespace std;
 
-void moveswapstr(string& empty, string & filled)
-{
-    //pseudo code, but you get the idea
-    size_t sz=empty.size();
-    const char *p= empty.data();
-    //move filled's resources to empty
-    empty.setsize(filled.size());
-    empty.setdata(filled.data());
-    //filled becomes empty
-    filled.setsize(sz);
-    filled.setdata(p);
-}
-
 class Movable
 {
     Movable (Movable&&); //move constructor

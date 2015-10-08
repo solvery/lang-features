a954aa986d4d0a576ccb508f2b51cd90c34e58df
Solvery <solvery@163.com>
2015-10-08 10:05:51 +0800

    cpp

--- cpp/move_1.cc
+++ cpp/move_1.cc
@@ -1,3 +1,5 @@
+// c++11
+// 右值引用
 
 #include <iostream>
 #include <algorithm>
@@ -6,6 +8,19 @@
 #include <vector>
 
 using namespace std;
+
+void moveswapstr(string& empty, string & filled)
+{
+    //pseudo code, but you get the idea
+    size_t sz=empty.size();
+    const char *p= empty.data();
+    //move filled's resources to empty
+    empty.setsize(filled.size());
+    empty.setdata(filled.data());
+    //filled becomes empty
+    filled.setsize(sz);
+    filled.setdata(p);
+}
 
 class Movable
 {

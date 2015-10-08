883fab510833165ce6b609462019ca22ab10b77e
Solvery <solvery@163.com>
2015-10-08 09:08:11 +0800

    cpp

--- cpp/c++11.lambda.cc
+++ cpp/c++11.lambda.cc
@@ -1,13 +1,30 @@
+// c++11
+// lambda, [capture](parameters)->return-type {body}
 
 #include <iostream>
 #include <algorithm>
 #include <numeric>
 #include <string>
 #include <vector>
+
 using namespace std;
- 
-vector num {2, -5, 9, 7, -2, 5, 3, 1, 0, -3, 8};
-vector p_num;
-copy_if(num.begin(), num.end(), back_inserter(p_num), [](int i){ return (i>0);} );
-int average = accumulate(p_num.begin(), p_num.end(), 0) / p_num.size();
-cout << "averge: " << average << endl;
+
+
+int main(int argc, char** arg) {
+    vector<int> num {2, -5, 9, 7, -2, 5, 3, 1, 0, -3, 8};
+    vector<int> p_num;
+    copy_if(num.begin(), num.end(), back_inserter(p_num), [](int i){ 
+            return (i>0);
+            } );
+    int average = accumulate(p_num.begin(), p_num.end(), 0) / p_num.size();
+    cout << "averge: " << average << endl;
+
+    char s[]="Hello World!";
+    int Uppercase = 0; //modified by the lambda
+    for_each(s, s+sizeof(s), [&Uppercase] (char c) {
+            if (isupper(c))
+            Uppercase++;
+            });
+    cout<< Uppercase<<" uppercase letters in: "<< s<<endl;
+}
+

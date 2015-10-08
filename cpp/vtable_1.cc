c054d21c31dee60b5edb91f3946a196a37f15712
Solvery <solvery@163.com>
2015-10-08 17:31:48 +0800

    cpp

--- cpp/vtable_1.cc
+++ cpp/vtable_1.cc
@@ -26,8 +26,19 @@ int main(int argc, char** arg) {
     cout << "addr vtable: \t\t\t" << (long*)(&b) << endl;
 
     // Invoke the first virtual function 
-    pFun = (Fun)*((int*)*(int*)(&b));
-    cout << "addr vtable function 1: \t" << &pFun << endl;
+    pFun = (Fun)*((long*)*(long*)(&b));
+
+    pFun = (Fun)*((long*)*(long*)(&b)+0);
     pFun();
+
+    pFun = (Fun)*((long*)*(long*)(&b)+1);
+    pFun();
+
+    pFun = (Fun)*((long*)*(long*)(&b)+2);
+    pFun();
+
+    cout << "addr vtable function 1: \t" << ((long*)*(long*)(&b)+0) << endl;
+    cout << "addr vtable function 2: \t" << ((long*)*(long*)(&b)+1) << endl;
+    cout << "addr vtable function 3: \t" << ((long*)*(long*)(&b)+2) << endl;
 }
 

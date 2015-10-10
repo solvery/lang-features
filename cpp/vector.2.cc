f371002f68b50bd653e6c3aa810e975f2ca7af4e
Solvery <solvery@163.com>
2015-10-10 14:11:10 +0800

    cpp

---
+++ cpp/vector.2.cc
@@ -0,0 +1,14 @@
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** arg) {
+    vector<int> v1;
+    vector<vector<int>> vv1;
+
+	v1.push_back(9);
+	vv1.push_back(v1);
+}
+

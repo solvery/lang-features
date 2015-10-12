77c5bc688463a66a52dd49ece9cdfbd19d8c22c7
Solvery <solvery@163.com>
2015-10-12 16:45:35 +0800

    cpp

--- cpp/generic.4.cc
+++ cpp/generic.4.cc
@@ -1,3 +1,4 @@
+// 模板无类型参数
 #include <iostream>
 using namespace std;
 
@@ -13,11 +14,23 @@ void loopIt(T x)
    }
 };
 
+template <typename T=float, int count=3>
+T multIt(T x)
+{
+   for(int ii=0; ii<count; ii++)
+   {
+       x = x * x;
+   }
+   return x;
+};
+
 main()
 {
    float xx = 2.1;
 
    loopIt<float,3>(xx);
+
+   cout << xx << ": " << multIt<>(xx) << endl;;
 }
 
 

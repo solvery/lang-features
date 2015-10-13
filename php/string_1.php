e23c56fff6a27cc14ce34e83885da6bc4683b551
Solvery <solvery@163.com>
2015-10-13 14:27:56 +0800

    php

---
+++ php/string_1.php
@@ -0,0 +1,18 @@
+
+<?php
+$word = "amet";
+
+$s = <<<EOF
+lorem ipsum
+dolor sit $word
+EOF;
+
+$count = 3;
+$item = "ball";
+echo "$count ${item}s\n";
+
+$fmt = "lorem %s %d %f";
+sprintf($fmt, "ipsum", 13, 3.7);
+
+?>
+

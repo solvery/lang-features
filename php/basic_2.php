b8a205116c039b2eb9b0a4926e26ea9fd53023d8
Solvery <solvery@163.com>
2015-10-13 14:20:01 +0800

    php

--- php/basic_2.php
+++ php/basic_2.php
@@ -1,8 +1,43 @@
 
 <?php
-$v = NULL;
-$a = [];
-$d = [];
-$x = 1;
-list($y, $z) = [2, 3];
+// variable
+{
+	$v = NULL;
+	$a = [];
+	$d = [];
+	$x = 1;
+	list($y, $z) = [2, 3];
+}
+{
+	list($g1, $g2) = [7, 8];
+
+	function swap_globals() {
+		global $g1, $g2;
+		list($g1, $g2) = [$g2, $g1];
+	}
+}
+{
+	define("PI", 3.14);
+	/// const PI = 3.14;
+}
+{
+	list($x, $y, $z) = [1 ,2, 3];
+	// 3 is discarded:
+	list($x, $y) = [1, 2, 3];
+	// z set to null
+	/// list($x, $y, $z) = [1, 2];
+}
+{
+	$x = 1;
+	$y = ++$x;
+	$z = --$y;
+	$x > 0 ? $x : -$x;
+}
+{
+	min(1, 2, 3);
+	max(1, 2, 3);
+	$a = [1, 2, 3];
+	min($a);
+	max($a);
+}
 ?>

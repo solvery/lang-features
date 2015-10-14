6bc205e70ea9a425c91c2fedd88bcf36e6b61704
Solvery <solvery@163.com>
2015-10-14 11:09:18 +0800

    php

---
+++ php/interface_1.php
@@ -0,0 +1,18 @@
+<?php
+
+interface I {
+	const N = 1;
+	function fun1();
+	function fun2($n, $m);
+}
+
+class A implements I {
+	function fun1(){}
+	function fun2($a,$b) {
+	}
+}
+
+echo "\n";
+?>
+
+

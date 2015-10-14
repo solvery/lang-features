74e72dbaa90549ad0df4193b1068ae93ee51bd7c
Solvery <solvery@163.com>
2015-10-14 10:41:47 +0800

    php

--- php/oo_1.php
+++ php/oo_1.php
@@ -6,15 +6,23 @@ class A {
 	protected $v3 = 3;
 
 	public function m1($n) {
-		echo $n;
+		echo $n; echo "\n";
 	}
 }
 
 class B extends A {
 	public $v4 = 4;
 	public function m2($n) {
+		echo $this-> v4; echo "\n";
+		echo $this-> v3; echo "\n";
 	}
 }
+
+$oa1 = new A();
+$oa1->m1(1);
+
+$ob1 = new B();
+$ob1->m2(1);
 
 echo "\n";
 ?>

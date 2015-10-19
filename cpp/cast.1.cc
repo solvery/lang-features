34164b427e69b827b03bb27d24a8f012aae7def9
Solvery <solvery@163.com>
2015-10-19 16:10:48 +0800

    cpp

--- cpp/cast.1.cc
+++ cpp/cast.1.cc
@@ -1,5 +1,6 @@
 // static_cast没有运行时的开销。
-// 多态类型转换，使用dynamic_cast可以进行运行时检查。
+// const_cast 增加或移除const属性。
+// dynamic_cast, 多态类型转换，可以进行运行时检查。
 //  
 #include <iostream>
 using namespace std;
@@ -7,7 +8,6 @@ using namespace std;
 class A {};
 class B : public A {};
 class C : public A {};
-
 
 int main(int argc, char** arg) {
 	{
@@ -18,6 +18,15 @@ int main(int argc, char** arg) {
 		const char *s1 = "hello";
 		char *s2 = const_cast<char*>(s1);
 		const char *s3 = const_cast<const char *>(s2);
+	}
+	{
+		int a = 10;
+		float f = 10;
+		struct NUM {
+			int v1;
+		};
+
+		/// NUM m = reinterpret_cast<NUM>(a);
 	}
 	A* p = new B();
 	//B* pb = dynamic_cast<B*>(pa);

1c1e8560fb37475433e4ba02034e2df930c1cf66
Solvery <solvery@163.com>
2015-10-19 16:00:42 +0800

    cpp

--- cpp/cast.1.cc
+++ cpp/cast.1.cc
@@ -1,3 +1,5 @@
+// static_cast没有运行时的开销。
+// 多态类型转换，使用dynamic_cast可以进行运行时检查。
 //  
 #include <iostream>
 using namespace std;
@@ -8,8 +10,12 @@ class C : public A {};
 
 
 int main(int argc, char** arg) {
-	A oa = A();
-	A* pa = &oa;
-	B* pb = dynamic_cast<B*>(pa);
+	{
+		double d = 3.14;
+		int i = static_cast<int>(d);
+	}
+	A* p = new B();
+	//B* pb = dynamic_cast<B*>(pa);
+	B* pb = static_cast<B*>(p);
 }
 

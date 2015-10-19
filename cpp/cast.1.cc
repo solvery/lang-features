97ee9d1ea87b978a9150f11e549edd95b19d1387
Solvery <solvery@163.com>
2015-10-19 15:43:25 +0800

    cpp

--- cpp/cast.1.cc
+++ cpp/cast.1.cc
@@ -1,2 +1,15 @@
-//  避免使用, 强制类型转换关闭或挂起了正常的类型检查
+//  
+#include <iostream>
+using namespace std;
+
+class A {};
+class B : public A {};
+class C : public A {};
+
+
+int main(int argc, char** arg) {
+	A oa = A();
+	A* pa = &oa;
+	B* pb = dynamic_cast<B*>(pa);
+}
 

14c865fa498b57a572c3c88ec78863ccbe18e2ea
solvery@163.com <solvery@163.com>
2016-09-26 17:21:57 +0800

    python

--- python/string.1.py
+++ python/string.1.py
@@ -6,5 +6,9 @@ for i in range(len(foo)):
     print foo[i], '(%d)' % i
 
 for d in foo:
-	print '%x' % struct.unpack('b', d)
+	d1 = struct.unpack('b', d)
+	print '%x' % d1
+	print type(d1[0])
+	if (d1[0] == 0x61):
+		print "match"
 

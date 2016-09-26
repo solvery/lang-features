522106d75f09898281cc7988a4cf7243b2617411
solvery@163.com <solvery@163.com>
2016-09-26 17:08:27 +0800

    python

--- python/file.1.py
+++ python/file.1.py
@@ -4,5 +4,9 @@ import struct
 binfile=open("file.1.in",'rb')    
 data = binfile.read()
 
-for d in data:
-	print d
+for i in range(0,len(data)):
+	print "%c" % data[i]
+	if (hex(data[i]) == '\x30'):
+		print "hah"
+
+
--- python/string.1.py
+++ python/string.1.py
@@ -1,5 +1,10 @@
 
+import struct
+
 foo = "abc"
 for i in range(len(foo)):
     print foo[i], '(%d)' % i
+
+for d in foo:
+	print '%x' % struct.unpack('b', d)
 

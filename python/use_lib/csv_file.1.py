02f9a5fab6f7a5151d440f817c34e7a3875a3500
Solvery Yang <solvery@163.com>
2016-10-06 00:09:35 +0800

    /Users/ciminy/workspace/code/lang-features/python/use_lib

--- python/use_lib/csv_file.1.py
+++ python/use_lib/csv_file.1.py
@@ -1,10 +1,10 @@
 
-file_in = 'csv_file.1.in'
-
 import csv
 
+#read csv type file.
+file_in = 'csv_file.1.in'
 with open(file_in,'rb') as f:
-    #read cvs type file.
-    reader = csv.reader(f)
-    for row in reader:
+    csv_contents = csv.reader(f)
+    for row in csv_contents:
         print row[3]
+

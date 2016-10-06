fc890658c58366cb1c973439a3a232afaa920694
Solvery Yang <solvery@163.com>
2016-10-06 09:31:18 +0800

    /workspace/code/lang-features/python/use_lib

--- python/use_lib/re.1.py
+++ python/use_lib/re.1.py
@@ -3,7 +3,7 @@ import re
 
 str1 = 'hello world! hello'
  
-# 将正则表达式编译成Pattern对象
+# 将正则表达式编译成Pattern对象, 工厂方法
 pattern = re.compile(r'hello')
  
 # 使用Pattern匹配文本，获得匹配结果，无法匹配时将返回None
@@ -13,3 +13,6 @@ if match:
     # 使用Match获得分组信息
     print match.group()
  
+# 一行完成
+m = re.match(r'hello', 'hello world!')
+print m.group()

c383f88e010f85a3e380320c66285bff94f93d93
Solvery Yang <solvery@163.com>
2016-10-06 23:20:59 +0800

    /workspace/code/lang-features/python/import.1

---
+++ python/import.1/import_2.py
@@ -0,0 +1,11 @@
+#encoding=utf-8
+a = 37                    # 一个变量
+
+def foo():                  # 一个函数
+    print "I'm foo"
+
+class bar:                # 一个类
+    def grok(self):
+        print "I'm bar.grok"
+
+b = bar()                 # 创建一个实例

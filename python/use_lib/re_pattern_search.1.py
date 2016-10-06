704e02f725f889f01ebb03e5b66028de5efe9e07
Solvery Yang <solvery@163.com>
2016-10-06 23:06:07 +0800

    /workspace/code/lang-features/python/use_lib

--- python/use_lib/re_pattern_search.1.py
+++ python/use_lib/re_pattern_search.1.py
@@ -2,7 +2,9 @@
 import re 
  
 # 将正则表达式编译成Pattern对象 
+#pattern = re.compile(r'hello') 
 pattern = re.compile(r'world') 
+#pattern = re.compile(r'.*world') 
  
 # 使用search()查找匹配的子串，不存在能匹配的子串时将返回None 
 # 这个例子中使用match()无法成功匹配  star
@@ -10,7 +12,7 @@ match1 = pattern.search('hello world!')
 match2 = pattern.match ('hello world!') 
  
 if match1 : 
-    print match1.group() 
+    print '1 ' + match1.group() 
 if match2 : 
-    print match2.group() 
+    print '2 ' + match2.group() 
 

3f6edf6a7fe5a00ac401f0e88f53bc0e0f5c80be
Solvery Yang <solvery@163.com>
2016-10-06 09:44:36 +0800

    /workspace/code/lang-features/python/use_lib

--- python/use_lib/re.1.py
+++ python/use_lib/re.1.py
@@ -1,18 +1,9 @@
 # encoding: UTF-8
+
 import re
 
-str1 = 'hello world! hello'
- 
-# 将正则表达式编译成Pattern对象, 工厂方法
-pattern = re.compile(r'hello')
- 
-# 使用Pattern匹配文本，获得匹配结果，无法匹配时将返回None
-match = pattern.match(str1)
- 
-if match:
-    # 使用Match获得分组信息
-    print match.group()
- 
-# 一行完成
+## 一行完成
 m = re.match(r'hello', 'hello world!')
 print m.group()
+
+#escape(string)，用于将string中的正则表达式元字符如*/+/?等之前加上转义符再返回

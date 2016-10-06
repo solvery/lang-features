9d560ac9d758ed3e337f511e30dc4bf7d94344e5
Solvery Yang <solvery@163.com>
2016-10-06 09:47:19 +0800

    /workspace/code/lang-features/python/use_lib

--- python/use_lib/re_pattern_search.1.py
+++ python/use_lib/re_pattern_search.1.py
@@ -5,7 +5,7 @@ import re
 pattern = re.compile(r'world') 
  
 # 使用search()查找匹配的子串，不存在能匹配的子串时将返回None 
-# 这个例子中使用match()无法成功匹配 
+# 这个例子中使用match()无法成功匹配  star
 match1 = pattern.search('hello world!') 
 match2 = pattern.match ('hello world!') 
  

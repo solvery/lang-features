b015adaff7e802158d5675ea67bf66e267cc4a04
Solvery Yang <solvery@163.com>
2016-10-06 09:27:48 +0800

    /workspace/code/lang-features/python/use_lib

---
+++ python/use_lib/re.1.py
@@ -0,0 +1,15 @@
+# encoding: UTF-8
+import re
+
+str1 = 'hello world! hello'
+ 
+# 将正则表达式编译成Pattern对象
+pattern = re.compile(r'hello')
+ 
+# 使用Pattern匹配文本，获得匹配结果，无法匹配时将返回None
+match = pattern.match(str1)
+ 
+if match:
+    # 使用Match获得分组信息
+    print match.group()
+ 

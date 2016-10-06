c3e3cc17d4d8922b95e75fc02458513ab625a2ba
Solvery Yang <solvery@163.com>
2016-10-06 09:44:40 +0800

    /workspace/code/lang-features/python/use_lib

---
+++ python/use_lib/re.3.py
@@ -0,0 +1,9 @@
+import re
+p = re.compile(r'(\w+) (\w+)(?P<sign>.*)', re.DOTALL)
+ 
+print "p.pattern:", p.pattern
+print "p.flags:", p.flags
+print "p.groups:", p.groups
+print "p.groupindex:", p.groupindex
+
+
---
+++ python/use_lib/re_pattern_search.1.py
@@ -0,0 +1,16 @@
+# encoding: UTF-8 
+import re 
+ 
+# 将正则表达式编译成Pattern对象 
+pattern = re.compile(r'world') 
+ 
+# 使用search()查找匹配的子串，不存在能匹配的子串时将返回None 
+# 这个例子中使用match()无法成功匹配 
+match1 = pattern.search('hello world!') 
+match2 = pattern.match ('hello world!') 
+ 
+if match1 : 
+    print match1.group() 
+if match2 : 
+    print match2.group() 
+

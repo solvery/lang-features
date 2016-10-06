597d9350fcf8597657fd22a25e9a985144be1072
Solvery Yang <solvery@163.com>
2016-10-06 23:06:58 +0800

    /Users/ciminy/workspace/code/lang-features/python/use_lib

--- python/use_lib/re_pattern_search.1.py
+++ python/use_lib/re_pattern_search.1.py
@@ -4,7 +4,7 @@ import re
 # 将正则表达式编译成Pattern对象 
 #pattern = re.compile(r'hello') 
 pattern = re.compile(r'world') 
-#pattern = re.compile(r'.*world') 
+#pattern = re.compile(r'.*world')  # 需要按照规则匹配
  
 # 使用search()查找匹配的子串，不存在能匹配的子串时将返回None 
 # 这个例子中使用match()无法成功匹配  star

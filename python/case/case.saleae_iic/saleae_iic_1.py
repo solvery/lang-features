1290e8947881a9d12fe017e082e3e123fa96783b
solvery@163.com <solvery@163.com>
2016-11-24 10:48:48 +0800

    case.saleae_iic

--- python/case/case.saleae_iic/saleae_iic_1.py
+++ python/case/case.saleae_iic/saleae_iic_1.py
@@ -2,9 +2,8 @@
 import csv
 
 #read csv type file.
-file_in = 'csv_file.1.in'
+file_in = '1.txt'
 with open(file_in,'rb') as f:
     csv_contents = csv.reader(f)
     for row in csv_contents:
-        print row[2]
-
+        print row[0]

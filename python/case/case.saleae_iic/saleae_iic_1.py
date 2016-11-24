6dcc27c96108de6156a0a3af197937d6bf6af8ef
solvery@163.com <solvery@163.com>
2016-11-24 11:17:56 +0800

    case.saleae_iic

--- python/case/case.saleae_iic/saleae_iic_1.py
+++ python/case/case.saleae_iic/saleae_iic_1.py
@@ -8,10 +8,11 @@ file_in = 'untitled.txt'
 sample_data_time = 0.0
 with open(file_in,'rb') as f:
     csv_contents = csv.reader(f)
-    headers = next(csv_contents)  
+    headers = next(csv_contents)   # 去掉第一行
     for row in csv_contents:
         sample_data_time_last = sample_data_time
         sample_data_time = float(row[0])
         if ( sample_data_time - sample_data_time_last) > 0.0002 :
             print
         print row[2], row[3], row[4], row[5]
+

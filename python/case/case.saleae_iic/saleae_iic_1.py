fc6bf58930615d9f61ae58128ab35a39d79a1437
solvery@163.com <solvery@163.com>
2016-11-24 10:46:20 +0800

    case.saleae_iic

---
+++ python/case/case.saleae_iic/saleae_iic_1.py
@@ -0,0 +1,10 @@
+
+import csv
+
+#read csv type file.
+file_in = 'csv_file.1.in'
+with open(file_in,'rb') as f:
+    csv_contents = csv.reader(f)
+    for row in csv_contents:
+        print row[2]
+

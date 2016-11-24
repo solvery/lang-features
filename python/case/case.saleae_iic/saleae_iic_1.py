f87488e06dfea2374e6b11deb32fe3b894ba4951
solvery@163.com <solvery@163.com>
2016-11-24 11:17:16 +0800

    case.saleae_iic

--- python/case/case.saleae_iic/saleae_iic_1.py
+++ python/case/case.saleae_iic/saleae_iic_1.py
@@ -8,6 +8,7 @@ file_in = 'untitled.txt'
 sample_data_time = 0.0
 with open(file_in,'rb') as f:
     csv_contents = csv.reader(f)
+    headers = next(csv_contents)  
     for row in csv_contents:
         sample_data_time_last = sample_data_time
         sample_data_time = float(row[0])

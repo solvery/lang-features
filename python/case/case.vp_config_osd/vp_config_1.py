03bca6d8c40de694c145a6b1f986e529e6dff333
solvery@163.com <solvery@163.com>
2016-12-16 12:09:43 +0800

    case.vp_config_osd

--- python/case/case.vp_config_osd/vp_config_1.py
+++ python/case/case.vp_config_osd/vp_config_1.py
@@ -4,71 +4,21 @@ import struct
 import time  
 
 #  COM3修改成您老的使用的串口 
-ser = serial.Serial("COM5", 115200)  
+ser = serial.Serial("COM3", 115200)  
 def main():  
-
-    pkg_len = 0
-    pkg_size = 254
-    pkg_sum = 0
-    pkg_cnt = 0
-    pkg_index = 0
-
-    # 读取数据到数组
-    with open("data.bin",'rb') as fd_in:
-        data_in = fd_in.read()
-
-    pkg_len = len(data_in)
-    print 'pkg len %d' % pkg_len
-
     while True:  
         count = ser.inWaiting()  
         if count != 0:  
             recv = ser.read(count)  
+            ser.write(recv)  
             
             for data in recv:
                 data_hex = struct.unpack('B', data)
-                if data_hex[0] == 0x0A:
-                    # 收到ack
-                    # 准备数据
-                    pkg_data = data_in[pkg_index:pkg_index+pkg_size];
-                    pkg_len  = len(pkg_data)
-
-                    # 文件末尾，补充补充数据
-                    if pkg_len<pkg_size:
-                        for i in range(pkg_len, pkg_size):
-                            pkg_data = pkg_data+ '\0'
-                            
-                    # 文件内容为0，退出
-                    if pkg_len == 0:
-                        return
-
-                    pkg_sum = cal_sum(pkg_data)
-
-                    print 'pkg_cnt = %d' % pkg_cnt ,
-                    print 'pkg_len = %d' % pkg_len ,
-                    print 'pkg_sum = %04X' % pkg_sum 
-
-                    # 发送数据
-                    pkg_sum_str = struct.pack('BB', pkg_sum/0x100, pkg_sum%0x100)
-                    pkg_data = pkg_data + pkg_sum_str
-                    ser.write(pkg_data)
-
-                    pkg_cnt = pkg_cnt + 1;
-                    pkg_index = pkg_index + pkg_size
-                      
-                if data_hex[0] == 0xA0:
-                    print 'get nak'
-            #print 
+                print '%02x' % data_hex,
+            print 
         ser.flushInput()  
         time.sleep(0.1)  
-
-def cal_sum(data):
-    data_sum = 0
-    for d in data:
-        d_hex = struct.unpack('B', d)
-        data_sum = data_sum + d_hex[0];
-    return data_sum
-
+     
 if __name__ == '__main__':  
     try:  
         main()  

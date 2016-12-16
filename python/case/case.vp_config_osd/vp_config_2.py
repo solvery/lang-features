ef0a8955241b32ba88f135b2296b592cd7fdf133
solvery@163.com <solvery@163.com>
2016-12-16 14:31:22 +0800

    case.vp_config_osd

--- python/case/case.vp_config_osd/vp_config_2.py
+++ python/case/case.vp_config_osd/vp_config_2.py
@@ -3,16 +3,29 @@ import serial
 import struct
 import time  
 
-config_data_hex = [0x80, 0x81, 0x82, 0x83, 0x00, 0x00, 0xc8, 0x00, 0xc8, 0x01, 0x90, 0x00, 0x50, 0x80, 0x23];
+config_head = [0x80, 0x81, 0x82, 0x83, 0x00]
+
+osd_left    = 200
+osd_top     = 200
+osd_seg_height  = 400
+osd_seg_width   = 80 
+osd_alpha   = 0x80
+osd_num     = 0x27
 
 #  COM3修改成您老的使用的串口 
 ser = serial.Serial("COM3", 115200)  
 def main():  
     data_bin_array=''
     while True:  
-        for data in config_data_hex:
+        for data in config_head:
             data_bin = struct.pack('B', data) 
             data_bin_array = data_bin_array + data_bin[0]
+        data_bin_array = data_bin_array + struct.pack('BB', osd_left/0x100, osd_left%0x100)
+        data_bin_array = data_bin_array + struct.pack('BB', osd_top/0x100, osd_top%0x100)
+        data_bin_array = data_bin_array + struct.pack('BB', osd_seg_height/0x100, osd_seg_height%0x100)
+        data_bin_array = data_bin_array + struct.pack('BB', osd_seg_width/0x100, osd_seg_width%0x100)
+        data_bin_array = data_bin_array + struct.pack('B', osd_alpha)
+        data_bin_array = data_bin_array + struct.pack('B', osd_num)
         ser.write(data_bin_array)
         ser.flushInput()  
         time.sleep(0.1)  

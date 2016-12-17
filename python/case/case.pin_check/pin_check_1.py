78b5ecf1c067ea9178c99c96f85085af02f125ea
solvery@163.com <solvery@163.com>
2016-12-17 15:03:32 +0800

    case.pin_check

--- python/case/case.pin_check/pin_check_1.py
+++ python/case/case.pin_check/pin_check_1.py
@@ -1,8 +1,15 @@
 
+
 pin_dict_1={}
 with open('pin1.txt','rb') as f:
     for line in f:
         pin_dict_1[line] = 1
 
-for key in pin_dict_1:
-    print key,
+pin_dict_2={}
+with open('pin2.txt','rb') as f:
+    for line in f:
+        pin_dict_2[line] = 1
+
+for key_pin1 in pin_dict_1:
+    if (key_pin1 not in pin_dict_2.keys()):
+        print key_pin1,

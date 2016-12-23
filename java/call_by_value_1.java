0b1845600f1f96fff61973c61f7d1a2c7d1adc65
solvery@163.com <solvery@163.com>
2016-12-23 14:28:09 +0800

    /home/yang/workspace/sw/lang-features/java at java

---
+++ java/call_by_value_1.java
@@ -0,0 +1,38 @@
+
+
+public class call_by_value_1 {
+    void func1(int value) {
+        value = 100;
+    }
+
+    void func2(String text) {
+        text = "windows";
+    }
+
+    void func3(StringBuilder builder) {
+        builder.append(" world");
+    }
+
+    void func4(StringBuilder builder) {
+        builder = new StringBuilder("hehe");
+    }
+
+    public static void main(String[] args) {
+        call_by_value_1 o1 = new call_by_value_1();
+        int num = 10;
+        o1.func1(num);
+        System.out.println(num);
+
+        String s = "hello";
+        o1.func2(s);
+        System.out.println(s);
+
+        StringBuilder sb = new StringBuilder("hello");
+        o1.func3(sb);
+        System.out.println(sb);
+
+        o1.func4(sb);
+        System.out.println(sb);
+    }
+}
+

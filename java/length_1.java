d5dd97d89a02d489b18cf79ef2a5c3a01c918044
solvery@163.com <solvery@163.com>
2016-12-24 10:51:20 +0800

    /home/yang/workspace/sw/lang-features/java at java

---
+++ java/length_1.java
@@ -0,0 +1,17 @@
+// length 针对数组
+// length 针对String 
+
+import java.util.*;
+
+public class length_1 {
+    public static void main(String[] args) {
+        int[] a1 = new int[10];
+        System.out.println(a1.length);
+        // System.out.println(a1.length()); // error
+
+        System.out.println("hello".length());
+        // System.out.println("hello".length); // error
+    }
+}
+
+

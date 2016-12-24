89f36217d7b258a71cd26d4f2164b5aa4adbe51a
solvery@163.com <solvery@163.com>
2016-12-24 14:52:04 +0800

    /home/yang/workspace/sw/lang-features/java at java

---
+++ java/char_1.java
@@ -0,0 +1,12 @@
+// java char类型可以用来存放汉子。
+
+public class char_1 {
+    public static void main(String[] args) {
+        char c='李';  
+        System.out.println(c);  
+        String s="周";  
+        System.out.println(s);  
+        System.out.println("");
+    }
+}
+

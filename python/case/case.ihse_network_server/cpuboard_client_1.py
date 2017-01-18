8abf951b7fa7b6d3c2f3680418cc03654b5bae1b
solvery@163.com <solvery@163.com>
2017-01-18 11:08:51 +0800

    case.ihse_network_server

--- python/case/case.ihse_network_server/cpuboard_client_1.py
+++ python/case/case.ihse_network_server/cpuboard_client_1.py
@@ -1,30 +1,43 @@
 import socket
+import struct 
+import time
 import sys
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('127.0.0.1', 5555)
+server_address = ('192.168.2.28', 5555)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
+
+def print_hex(data):
+    for d in data:
+        print '%02x' % struct.unpack('B', d),
+    print
+    return
+
+def hex2bin(data):
+    data_bin_array=''
+    for d in data:
+        data_bin_array = data_bin_array + struct.pack('B', d)
+    return data_bin_array
+
+
+data_send1 = hex2bin([0x1b, 0x28, 0x53])
 
 try:
     
     # Send data
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+    print_hex(data_send1)
+    sock.sendall(data_send1)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
+    data = sock.recv(8192)
+    print_hex(data)
+    while True:
+        time.sleep(50)
 
 finally:
     print >>sys.stderr, 'closing socket'
     sock.close()
+

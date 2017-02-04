ff339b584fcb1030bec06e07078340efff4a6e0a
solvery@163.com <solvery@163.com>
2017-02-04 20:23:11 +0800

    syslog_1

--- python/network/syslog_1/server.py
+++ python/network/syslog_1/server.py
@@ -8,15 +8,13 @@ addr = (server,port)
 TCPSock = socket(AF_INET,SOCK_DGRAM)
 TCPSock.bind(addr)
 
-db=open("receive.log", "w")
-
 while 1:
     data,addr = TCPSock.recvfrom(buf)
     if not data:
         print ("No response from systems!")
         break
     else:
-	    print (data)
-		#print ("Message: ", data, file=db, flush=True)
+        print (data)
+        #print ("Message: ", data, file=db, flush=True)
 
 TCPSock.close()

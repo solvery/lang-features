ec9e5b5f6d4f53e60ca3dc953dd25c61de347c2b
solvery@163.com <solvery@163.com>
2017-02-06 20:02:02 +0800

    case.sort_1-24

--- python/case/case.sort_1-24/sort_1-24.py
+++ python/case/case.sort_1-24/sort_1-24.py
@@ -2,4 +2,5 @@
 for x in range(1,24+1):
     for y in range(1,24+1):
         val = float(x)/y
-        print ("%02.08f = %d/%d" % (val, x, y))
+        val_x27 = val *27 
+        print ("%02.08f = %02d/%02d    %02.08f" % (val, x, y, val_x27))

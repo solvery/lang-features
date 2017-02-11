8a89b57e301af005fbdf3f9bd2e550bf4ee1cd21
solvery@163.com <solvery@163.com>
2017-02-11 10:40:57 +0800

    socket_1

---
+++ python/network/socket_1/socket_1.py
@@ -0,0 +1,10 @@
+import socket  
+  
+def print_machine_info():  
+    host_name = socket.gethostname()  
+    ip_address = socket.gethostbyname(host_name)  
+    print ("host name is %s " %host_name)  
+    print ("ip address is %s" %ip_address)  
+  
+if __name__ == '__main__':  
+    print_machine_info()

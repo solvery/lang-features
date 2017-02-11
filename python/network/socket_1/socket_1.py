77542f3273e5375b62c740ab4f9ca76fd9429a86
solvery@163.com <solvery@163.com>
2017-02-11 10:43:32 +0800

    socket_1

--- python/network/socket_1/socket_1.py
+++ python/network/socket_1/socket_1.py
@@ -6,5 +6,18 @@ def print_machine_info():
     print ("host name is %s " %host_name)  
     print ("ip address is %s" %ip_address)  
   
+def convert_integer():  
+    data = 1234  
+    # 32 bit  
+    print("original: %s => Long host byte order: %s, Network byte order: %s"  
+        % (data, socket.ntohl(data), socket.htonl(data)))  
+    # 16 bit  
+    print("original: %s => Short host byte order: %s, Network byte order: %s"  
+        % (data, socket.ntohs(data), socket.htons(data)))  
+    data1 = socket.htonl(data)  
+    data2 = socket.ntohl(data1)  
+    print("%s %s %s" %(data,data1,data2))  
+
 if __name__ == '__main__':  
     print_machine_info()
+    convert_integer()

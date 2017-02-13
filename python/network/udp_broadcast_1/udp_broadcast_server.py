3680c210f1e4627f0965f6ef06845fc95d4dd2f0
solvery@163.com <solvery@163.com>
2017-02-13 18:20:12 +0800

    udp_broadcast_1

--- python/network/udp_broadcast_1/udp_broadcast_server.py
+++ python/network/udp_broadcast_1/udp_broadcast_server.py
@@ -1,7 +1,7 @@
 import socket
 
 host=''
-port=10000
+port=5556
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -12,7 +12,7 @@ while 1:
     try:
         data,addr=s.recvfrom(1024)
         print "got data from",addr
-        s.sendto("broadcasting",addr)
         print data
+        #s.sendto("broadcasting",addr)
     except KeyboardInterrupt:
         raise

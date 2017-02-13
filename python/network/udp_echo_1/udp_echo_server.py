fd5f75866c96ffd4e731abacae6eb9f1c4bb115c
solvery@163.com <solvery@163.com>
2017-02-13 15:46:47 +0800

    udp_echo_1

---
+++ python/network/udp_echo_1/udp_echo_server.py
@@ -0,0 +1,17 @@
+import socket
+
+host=''
+port=10000
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+s.bind((host,port))
+
+while 1:
+    try:
+        data,addr=s.recvfrom(1024)
+        print "got data from",addr
+        s.sendto("broadcasting",addr)
+        print data
+    except KeyboardInterrupt:
+        raise

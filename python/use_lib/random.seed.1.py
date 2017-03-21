d463e845c2d5b1e246238d7350d765d8149ed4b8
solvery@163.com <solvery@163.com>
2017-03-21 11:11:51 +0800

    use_lib

---
+++ python/use_lib/random.seed.1.py
@@ -0,0 +1,10 @@
+
+import random
+
+random.seed(1)
+print random.randint(1,500)
+print random.randint(1,100)/100.0
+print random.randint(1,100)/100.0
+print random.randint(1,100)/100.0
+print random.randint(1,100)/100.0
+

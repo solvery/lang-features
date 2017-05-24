a4c8b63cbbdfc48cfe78e19aff0df9fcc9e7c818
solvery@163.com <solvery@163.com>
2017-05-24 19:04:29 +0800

    use_lib

--- python/case/case.dkm_api_sender/dkm_api_sender_uart.py
+++ python/case/case.dkm_api_sender/dkm_api_sender_uart.py
@@ -4,8 +4,42 @@ import serial
 import struct
 import time  
 
+cmd_get_system_time = [0x1b, 0x28, 0x53]
+
 #  COM3修改成您老的使用的串口 
 ser = serial.Serial("COM3", 115200)  
+
+
+def get_system_time()
+    uart_send(hex2bin(cmd_get_system_time))
+    recv = uart_recv()
+    recv_bytes = bin2hex(recv)
+    
+
+def uart_send(data):
+    ser.write(data)  
+
+def uart_recv():
+    while True:  
+        ser.flushInput()
+        count = ser.inWaiting()  
+        if count != 0:  
+            recv = ser.read(count)  
+            return recv
+        time.sleep(0.1)  
+
+def bin2hex(data_bin):
+    for d in data_bin:
+        data_hex = data_hex + struct.unpack('B', d)
+    return data_hex
+
+def hex2bin(data_hex):
+    data_bin=''
+    for d in data_hex:
+        data_bin = data_bin + struct.pack('B', d)
+    return data_bin
+
+
 def main():  
     while True:  
         count = ser.inWaiting()  
--- python/use_lib/logging_1.py
+++ python/use_lib/logging_1.py
@@ -2,16 +2,18 @@
 
 import logging
 
+formatter="%(asctime)s %(levelname)s %(message)s"
+
 logging.basicConfig(
         filename="config.log",
         filemode="w",
-        format="%(asctime)s-%(name)s-%(levelname)s-%(message)s",
+        format=formatter,
         level=logging.INFO);
 
 #定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 

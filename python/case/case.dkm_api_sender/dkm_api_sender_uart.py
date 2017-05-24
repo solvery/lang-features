e1b55a21b4c38a95659cec3b54917528b418dc08
solvery@163.com <solvery@163.com>
2017-05-24 18:51:08 +0800

    case.dkm_api_sender

---
+++ python/case/case.dkm_api_sender/dkm_api_sender_uart.py
@@ -0,0 +1,30 @@
+#encoding=utf-8
+
+import serial  
+import struct
+import time  
+
+#  COM3修改成您老的使用的串口 
+ser = serial.Serial("COM3", 115200)  
+def main():  
+    while True:  
+        count = ser.inWaiting()  
+        if count != 0:  
+            recv = ser.read(count)  
+            ser.write(recv)  
+            
+            for data in recv:
+                data_hex = struct.unpack('B', data)
+                print '%02x' % data_hex,
+            print 
+        ser.flushInput()  
+        time.sleep(0.1)  
+     
+if __name__ == '__main__':  
+    try:  
+        main()  
+    except KeyboardInterrupt:  
+        if ser != None:  
+            ser.close()  
+
+

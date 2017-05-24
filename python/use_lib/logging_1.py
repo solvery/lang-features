afe8ec17bf350ddf2306fb2b347a2d11014edca8
solvery@163.com <solvery@163.com>
2017-05-24 16:44:56 +0800

    use_lib

--- python/use_lib/logging_1.py
+++ python/use_lib/logging_1.py
@@ -2,11 +2,11 @@
 
 import logging
 
-logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename='myapp.log',
-                filemode='w')
+logging.basicConfig(
+        filename="config.log",
+        filemode="w",
+        format="%(asctime)s-%(name)s-%(levelname)s-%(message)s",
+        level=logging.INFO);
 
 #定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
 console = logging.StreamHandler()

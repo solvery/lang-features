9d8c82e81d7d79df4b7dca56e8b536c30b68903f
solvery@163.com <solvery@163.com>
2017-05-31 20:08:06 +0800

    use_lib

---
+++ python/use_lib/asyncio_2.py
@@ -0,0 +1,19 @@
+# python3
+import threading
+import asyncio
+
+async def hello2():
+    print("Hello world!")
+    r = await asyncio.sleep(1)
+    print("Hello again!")
+
+@asyncio.coroutine
+def hello():
+    print('Hello world! (%s)' % threading.currentThread())
+    yield from asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()

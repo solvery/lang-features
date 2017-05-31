2b6b84af893f3d7aa4096fc335b26ac0b063bfe4
solvery@163.com <solvery@163.com>
2017-05-31 19:26:31 +0800

    use_lib

--- python/use_lib/os_1.py
+++ python/use_lib/os_1.py
@@ -4,3 +4,6 @@ import os
 print os.name
 print os.getcwd()
 print os.listdir('.')
+
+print [x for x in os.listdir('.') if os.path.isdir(x)]
+print [x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1]=='.py']

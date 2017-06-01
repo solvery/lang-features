5c9a6d8bf1708f0af59023f7f4b7a255abf95e9b
solvery@163.com <solvery@163.com>
2017-06-01 09:59:30 +0800

    python

---
+++ python/use_lib/serial_win_3.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*  
+import serial  
+import struct
+import time  
+
+serial_port = sys.argv[1]
+ser = serial.Serial(serial_port, 115200)
+
+# logging
+formatter="%(asctime)s %(levelname)-12s %(message)s"
+
+# to file
+log_filename="uart.log"
+#logging.basicConfig( filename=log_filename, filemode="a", format=formatter, level=logging.INFO);
+
+# to console
+formatter = logging.Formatter(formatter)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+def main():  
+    while True:  
+        count = ser.inWaiting()  
+        if count != 0:  
+            recv = ser.read(count)  
+            ser.write(recv)  
+            
+            for data in recv:
+                data_hex = struct.unpack('B', data)
+                print '%02x' % data_hex,
+            print 
+        ser.flushInput()  
+        time.sleep(0.1)  
+     
+if __name__ == '__main__':  
+    try:  
+        main()  
+    except KeyboardInterrupt:  
+        if ser != None:  
+            ser.close()  
+
+

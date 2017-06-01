8939c39d98b85af4985c2d51d147f60a6f298542
solvery@163.com <solvery@163.com>
2017-06-01 10:49:36 +0800

    python

--- python/use_lib/random.1.py
+++ python/use_lib/random.1.py
@@ -2,4 +2,6 @@
 import random
 
 print random.randint(1,500)
+print random.randint(1,500)
+print random.randint(1,500)
 print random.randint(1,100)/100.0
---
+++ python/xrange_1.py
@@ -0,0 +1,5 @@
+#encoding=utf-8
+# xrange生成的不是一个数组，而是一个生成器。
+
+print (xrange(5))
+print list(xrange(5))

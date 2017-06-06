e65002f764acb19367e785fd3e74b62b52ba76a6
solvery <solvery@163.com>
2017-06-06 10:29:25 +0800

    D:\work_code\lang-features\windows_bat\case\case_xilinx_download

---
+++ windows_bat/case/case_xilinx_download/bit2mcs.bat
@@ -0,0 +1,4 @@
+
+set filename=system_top
+D:\Xilinx\14.7\ISE_DS\ISE\bin\nt64\promgen -s 131072 -u 0 %filename%.bit -spi -w -p mcs -o %filename%.mcs
+
---
+++ windows_bat/case/case_xilinx_download/mcs2bin.bat
@@ -0,0 +1,4 @@
+
+set filename=system_top
+D:\Xilinx\14.7\ISE_DS\ISE\bin\nt64\promgen -w -p bin -r %filename%.mcs -o %filename%.bin
+

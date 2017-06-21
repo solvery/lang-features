927749f148ba4decc9b1fd8faf3a1019912656be
solvery@163.com <solvery@163.com>
2017-06-21 15:08:03 +0800

    case.gen_aptovision_mac_bin

--- python/case/case.gen_aptovision_mac_bin/gen_icron_eeprom_mac.sh
+++ python/case/case.gen_aptovision_mac_bin/gen_icron_eeprom_mac.sh
@@ -10,15 +10,15 @@ python gen_icron_eeprom_mac_head.py 0x01 0x08 && cat icron_mac_head.bin icron_de
 python gen_icron_eeprom_mac_head.py 0x01 0x09 && cat icron_mac_head.bin icron_demo_rx_body.bin > icron_eeprom_rx_mac_09.bin
 python gen_icron_eeprom_mac_head.py 0x01 0x10 && cat icron_mac_head.bin icron_demo_rx_body.bin > icron_eeprom_rx_mac_10.bin
 
-python gen_icron_eeprom_mac_head.py 0x01 0x00 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_00.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x01 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_01.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x02 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_02.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x03 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_03.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x04 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_04.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x05 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_05.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x06 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_06.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x07 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_07.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x08 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_08.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x09 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_09.bin
-python gen_icron_eeprom_mac_head.py 0x01 0x10 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_10.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x00 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_00.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x01 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_01.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x02 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_02.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x03 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_03.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x04 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_04.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x05 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_05.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x06 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_06.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x07 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_07.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x08 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_08.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x09 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_09.bin
+python gen_icron_eeprom_mac_head.py 0x02 0x10 && cat icron_mac_head.bin icron_demo_tx_body.bin > icron_eeprom_tx_mac_10.bin
 

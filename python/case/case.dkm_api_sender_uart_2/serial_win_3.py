15a158f6d5ac2dab5361aebb4e77d5b54182be7a
solvery@163.com <solvery@163.com>
2017-08-03 19:51:04 +0800

    case.dkm_api_sender_uart_2

--- python/case/case.dkm_api_sender_uart_2/serial_win_3.py
+++ python/case/case.dkm_api_sender_uart_2/serial_win_3.py
@@ -36,22 +36,30 @@ def uart_recv():
                 buf = ser.read(count)
                 data += buf
                 if data != '':
-                    logging.info("recv: ".join(("%02x" % struct.unpack('B', n)) for n in data))
+                    logging.info("recv: " + (" ".join(("%02x" % struct.unpack('B', n)) for n in data)))
 
 def random_package():
     len = random.randint(2,30)
-    rand_list = [0x1b, 0x00, 0x00, len]
+    c1 = random.randint(0,255)
+    c2 = random.randint(0,255)
+    rand_list = [0x1b, 0x5b, 0x48, len]
     for i in range(len):
         rand_list +=[random.randint(1,255)]
     return rand_list
+
+def print_hex(data):
+    logging.info(" ".join(("%02x" % n) for n in data))
 
 def main():  
     t = threading.Thread(target=uart_recv)
     t.start()
     while True:  
         #ser.write('hello')
-        ser.write(bytearray(random_package()))
-        time.sleep(0.5)  
+        cmd = bytearray(random_package())
+        ser.write(cmd)
+        logging.info("send: ")
+        print_hex(cmd)
+        time.sleep(0.1)  
      
 if __name__ == '__main__':  
     try:  

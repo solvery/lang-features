095a6a64753f1f50c4a5d1f745e9cc3fd868e88e
solvery@163.com <solvery@163.com>
2017-08-07 10:16:35 +0800

    case.dkm_api_sender_tcp_1

--- python/case/case.dkm_api_sender_tcp_1/dkm_api_sender_tcp_1.py
+++ python/case/case.dkm_api_sender_tcp_1/dkm_api_sender_tcp_1.py
@@ -18,7 +18,7 @@ logging.basicConfig( filename=log_filename, filemode="a", format=formatter, leve
 # to console
 formatter = logging.Formatter(formatter)
 console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
+console.setLevel(logging.INFO)
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
@@ -31,6 +31,18 @@ ip_addr = sys.argv[1]
 server_address = (ip_addr, 5555)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
+
+def data_recv():
+    while True:  
+        data = ''
+        for i in range(1,10):
+            time.sleep(0.2)
+            data = sock.recv(8192)
+            count = len(data)
+            if count != 0:
+                if data != '':
+                    logging.info("recv: " + (" ".join(("%02x" % struct.unpack('B', n)) for n in data)))
+
 
 def random_package():
     len = random.randint(100,255)
@@ -50,16 +62,88 @@ def hex2bin(data_hex):
         data_bin = data_bin + struct.pack('B', d)
     return data_bin
 
-def data_recv():
-    while True:  
-        data = ''
-        for i in range(1,10):
-            time.sleep(0.2)
-            data = sock.recv(8192)
-            count = len(data)
-            if count != 0:
-                if data != '':
-                    logging.info("recv: " + (" ".join(("%02x" % struct.unpack('B', n)) for n in data)))
+def send_cmd(cmd):
+    ser.write(cmd)
+    logging.info("send: ")
+    print_hex(cmd)
+    time.sleep(1.1)  
+
+def random_cmd():
+    logging.info(sys._getframe().f_code.co_name)
+    cmd = bytearray(random_package())
+    send_cmd(cmd)
+
+def get_system_time():
+    logging.info(sys._getframe().f_code.co_name)
+    cmd = bytearray([0x1b, 0x28, 0x53])
+    send_cmd(cmd)
+
+def get_cpu_to_con():
+    logging.info(sys._getframe().f_code.co_name)
+    conid = random.randint(3000,3020)
+    cmd = [0x1b, 0x5b, 0x48, 0x07, 0x00, conid%0x100, conid/0x100]
+    send_cmd(bytearray(cmd))
+
+def get_con_to_cpu():
+    logging.info(sys._getframe().f_code.co_name)
+    cpuid = random.randint(1000,1020)
+    cmd = [0x1b, 0x5b, 0x4c, 0x07, 0x00, cpuid%0x100, cpuid/0x100]
+    send_cmd(bytearray(cmd))
+
+def get_all_connections():
+    logging.info(sys._getframe().f_code.co_name)
+    cmd = [0x1b, 0x5b, 0x52]
+    send_cmd(bytearray(cmd))
+
+def get_con_to_cpus():
+    logging.info(sys._getframe().f_code.co_name)
+    cpuid_list_len = random.randint(1,510)
+    cpuid_list = []
+    for i in range(cpuid_list_len):
+        cpuid_list += [random.randint(1001, 1999)]
+    cpu_cnt = len(cpuid_list)
+    cmd_size = 7+cpu_cnt*2
+    cmd = [0x1b, 0x5b, 0x4e, cmd_size%0x100, cmd_size/0x100, cpu_cnt%0x100, cpu_cnt/0x100]
+    for i in range(cpu_cnt):
+        cpu = [cpuid_list[i]%0x100, cpuid_list[i]/0x100]
+        cmd += cpu
+    logging.info("cmd len: %d" % len(cmd))
+    send_cmd(bytearray(cmd))
+
+def get_cpu_to_cons():
+    logging.info(sys._getframe().f_code.co_name)
+    conid_list_len = random.randint(1,510)
+    conid_list = []
+    for i in range(conid_list_len):
+        conid_list += [random.randint(1001, 1999)]
+    con_cnt = len(conid_list)
+    cmd_size = 7+con_cnt*2
+    cmd = [0x1b, 0x5b, 0x4a, cmd_size%0x100, cmd_size/0x100, con_cnt%0x100, con_cnt/0x100]
+    for i in range(con_cnt):
+        cpu = [conid_list[i]%0x100, conid_list[i]/0x100]
+        cmd += cpu
+    logging.info("cmd len: %d" % len(cmd))
+    send_cmd(bytearray(cmd))
+
+def get_con_list():
+    logging.info(sys._getframe().f_code.co_name)
+    cmd = [0x1b, 0x5b, 0x68, 0x07, 0x00, 0x00, 0x00]
+    send_cmd(bytearray(cmd))
+
+def get_user_list():
+    logging.info(sys._getframe().f_code.co_name)
+    cmd = [0x1b, 0x5b, 0x69, 0x07, 0x00, 0x00, 0x00]
+    send_cmd(bytearray(cmd))
+
+def get_cpu_list():
+    logging.info(sys._getframe().f_code.co_name)
+    cmd = [0x1b, 0x5b, 0x67, 0x07, 0x00, 0x00, 0x00]
+    send_cmd(bytearray(cmd))
+
+
+case_list1 = [random_cmd, get_system_time, get_cpu_to_con, get_con_to_cpu, get_cpu_to_cons, get_con_to_cpus, get_con_list, get_user_list, get_cpu_list]
+case_list2 = [get_cpu_list]
+case_list = case_list1
 
 def send_cmd(cmd):
     sock.sendall(cmd)
@@ -76,13 +160,8 @@ def main():
     t.start()
     try:
         while True:  
-            #ser.write('hello')
-            cmd = bytearray(random_package())
-            send_cmd(cmd)
-            cmd = bytearray([0x1b, 0x28, 0x53])
-            send_cmd(cmd)
-            cmd = bytearray([0x1b, 0x5b, 0x41])
-            send_cmd(cmd)
+            random.choice(case_list)()
+            time.sleep(1.5)  
     finally:
         print >>sys.stderr, 'closing socket'
         sock.close()

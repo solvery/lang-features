2316d85faade5237d057c7bb7a362d4f7f634767
solvery@163.com <solvery@163.com>
2017-08-24 15:54:49 +0800

    case.uart2hid_send_random_key

---
+++ python/case/case.uart2hid_send_random_key/gen_rand_key_file_1.py
@@ -0,0 +1,19 @@
+
+import random
+
+list_char = []
+for i in range(48,58):
+    list_char += chr(i)
+for i in range(65,91):
+    list_char += chr(i)
+for i in range(97,123):
+    list_char += chr(i)
+
+print list_char
+
+with open ('data.txt', 'w') as fd:
+    for i in range(100):
+        l = random.shuffle(list_char)
+        s = ''.join(list_char)
+        s += '\n'
+        fd.write(s)

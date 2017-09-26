042e0d6b6dd48548dcaa7af299578f49f51d2dc7
solvery@163.com <solvery@163.com>
2017-09-26 17:14:37 +0800

    case.aptovision_api_1

--- python/case/case.aptovision_api_1/aptovision_api_1.py
+++ python/case/case.aptovision_api_1/aptovision_api_1.py
@@ -1,23 +1,32 @@
 import time
-
+import json
 
 def nt_read(tn):
-    ret = tn.read_until('{')
-    ret += tn.read_very_eager()
-    print "read: \n" + ret 
+    s = tn.read_until('{')
+    s += tn.read_very_eager()
+    print "read: \n" + s
+    return s
     
 def do_telnet():
     import telnetlib
-    host='127.0.0.1'
+    host='192.168.2.7'
     port=6970
     tn = telnetlib.Telnet(host, port, timeout=20)
     tn.set_debuglevel(2)
 
     tn.write("mode human on" + '\n')
+    json_str = nt_read(tn)
     tn.write("require blueriver_api 2.11.0" + '\n')
+    json_str = nt_read(tn)
 
     tn.write("test ALL memory" + '\r')
-    nt_read(tn)
+    json_str = nt_read(tn)
+    json_parsed = json.loads(json_str)
+    request_id = str(json_parsed['request_id'])
+    
+    time.sleep(2)
+    tn.write('request ' + request_id + '\r\n')
+    json_str = nt_read(tn)
 
     tn.close() 
 

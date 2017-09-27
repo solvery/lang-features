455594e3aa0c37d14276e26b237b01f6f4b5d32b
solvery@163.com <solvery@163.com>
2017-09-27 14:19:06 +0800

    case.aptovision_api_1

--- python/case/case.aptovision_api_1/aptovision_api_1.py
+++ python/case/case.aptovision_api_1/aptovision_api_1.py
@@ -1,16 +1,18 @@
+import sys
 import time
 import json
 
 def nt_read(tn):
     s = tn.read_until('{')
     s += tn.read_very_eager()
-    print "read: \n" + s
+    #print "read: \n" + s
     return s
     
 def do_telnet():
     import telnetlib
     #host='127.0.0.1'
-    host='192.168.2.7'
+    ip_addr = sys.argv[1]
+    host=ip_addr
     port=6970
     tn = telnetlib.Telnet(host, port, timeout=20)
     tn.set_debuglevel(0)

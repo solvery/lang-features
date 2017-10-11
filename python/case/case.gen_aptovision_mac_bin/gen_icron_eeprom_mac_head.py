dd570cad9631af1295cf029e85aa677b16034b3b
solvery@163.com <solvery@163.com>
2017-10-11 17:42:28 +0800

    case.gen_aptovision_mac_bin

--- python/case/case.gen_aptovision_mac_bin/gen_icron_eeprom_mac_head.py
+++ python/case/case.gen_aptovision_mac_bin/gen_icron_eeprom_mac_head.py
@@ -1,17 +1,22 @@
 
 import struct
 import sys
+import os
 
-mac_p1 = int(sys.argv[1], 0)
-mac_p2 = int(sys.argv[2], 0)
+def gen_mac_head(mac_p1, mac_p2):
+    mac_addr = [0x01, 0xd8, 0x80, 0x30, mac_p1, 0x00, mac_p2]
+    data_bin_array=''
+    for data in mac_addr:
+        data_bin = struct.pack('B', data) 
+        data_bin_array = data_bin_array + data_bin[0];
+    return data_bin_array
 
-mac_addr = [0x01, 0x34, 0x1b, 0x22, 0x80, mac_p1, mac_p2]
-data_bin_array=''
-for data in mac_addr:
-    data_bin = struct.pack('B', data) 
-    data_bin_array = data_bin_array + data_bin[0];
+fn_tx=open("icron_demo_tx_body.bin",'rb').read()
+fn_rx=open("icron_demo_rx_body.bin",'rb').read()
+for i in range(1,4+1):
+    fn = "icron_eeprom_tx_mac_" + str(i) + ".bin"
+    data_bin_array = gen_mac_head(0xA2, i) + fn_tx
+    with open(fn,'wb') as fd:
+        fd.write(data_bin_array)
 
-
-with open("icron_mac_head.bin",'wb') as fd:
-    fd.write(data_bin_array)
 

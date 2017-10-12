c250f6dfa9f91600f4ab6ceee45156f605f28b92
solvery@163.com <solvery@163.com>
2017-10-12 10:04:22 +0800

    case.gen_aptovision_mac_bin

--- python/case/case.gen_aptovision_mac_bin/gen_icron_eeprom_mac_head.py
+++ python/case/case.gen_aptovision_mac_bin/gen_icron_eeprom_mac_head.py
@@ -3,6 +3,8 @@ import struct
 import sys
 import os
 
+num=4
+
 def gen_mac_head(mac_p1, mac_p2):
     mac_addr = [0x01, 0xd8, 0x80, 0x30, mac_p1, 0x00, mac_p2]
     data_bin_array=''
@@ -11,12 +13,19 @@ def gen_mac_head(mac_p1, mac_p2):
         data_bin_array = data_bin_array + data_bin[0];
     return data_bin_array
 
-fn_tx=open("icron_demo_tx_body.bin",'rb').read()
-fn_rx=open("icron_demo_rx_body.bin",'rb').read()
-for i in range(1,4+1):
+fn_tx_body=open("icron_demo_tx_body.bin",'rb').read()
+fn_rx_body=open("icron_demo_rx_body.bin",'rb').read()
+for i in range(1,num+1):
     fn = "icron_eeprom_tx_mac_" + str(i) + ".bin"
-    data_bin_array = gen_mac_head(0xA2, i) + fn_tx
+    data_tx = gen_mac_head(0xA2, i) + fn_tx_body
     with open(fn,'wb') as fd:
-        fd.write(data_bin_array)
+        fd.write(data_tx)
+
+    fn = "icron_eeprom_rx_mac_" + str(i) + ".bin"
+    data_rx = gen_mac_head(0xB2, i) + fn_tx_body
+    with open(fn,'wb') as fd:
+        fd.write(data_rx)
+
+
 
 

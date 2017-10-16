53633bd9e22f2b005e2eace793b17955532e070d
solvery@163.com <solvery@163.com>
2017-10-16 19:44:43 +0800

    case.apc8641_power_1

--- python/case/case.apc8641_power_1/apc8641_power_1.py
+++ python/case/case.apc8641_power_1/apc8641_power_1.py
@@ -1,12 +1,23 @@
 #encoding=utf-8
+import sys
+import telnetlib
+import time
 
-def do_telnet(Host, username, password, finish, commands):
-    import telnetlib
-    import time
- 
-    tn = telnetlib.Telnet(Host, port=23, timeout=20)
-    tn.set_debuglevel(2)
-     
+port        = sys.argv[1]
+delay_on    = int(sys.argv[2])
+delay_off   = int(sys.argv[3])
+
+# 配置选项
+Host = '192.168.2.108'
+username = 'apc'
+password = 'apc'
+finish = '>'
+commands = ["whoami"]
+
+tn = telnetlib.Telnet(Host, port=23, timeout=20)
+tn.set_debuglevel(0)
+
+def do_telnet():
     tn.read_until('User Name : ')
     tn.write(username + '\r')
     
@@ -15,21 +26,20 @@ def do_telnet(Host, username, password, finish, commands):
       
     tn.read_until(finish)
     while True:
-        tn.write('%s\r' % 'olOn 1')
+        tn.write('olOn ' + port + '\r')
         tn.read_until(finish)
-        time.sleep(2)
-        tn.write('%s\r' % 'olOff 1')
+        time.sleep(delay_on)
+
+        tn.write('olOff ' + port + '\r')
         tn.read_until(finish)
-        time.sleep(2)
+        time.sleep(delay_off)
     
     tn.read_until(finish)
     tn.close() # tn.write('exit\n')
 
 if __name__=='__main__':
-	 # 配置选项
-	Host = '192.168.2.108'
-	username = 'apc'
-	password = 'apc'
-	finish = '>'
-	commands = ["whoami"]
-	do_telnet(Host, username, password, finish, commands)
+    try:
+        do_telnet()
+    except KeyboardInterrupt:
+        tn.write('%s\r' % 'olOff ' + port)
+        tn.close() 

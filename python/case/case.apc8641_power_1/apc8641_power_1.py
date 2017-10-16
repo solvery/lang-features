1005ceacfb411d7cb04c7a372eb9be0eb8d90ee4
solvery@163.com <solvery@163.com>
2017-10-16 19:56:28 +0800

    case.apc8641_power_1

--- python/case/case.apc8641_power_1/apc8641_power_1.py
+++ python/case/case.apc8641_power_1/apc8641_power_1.py
@@ -2,6 +2,8 @@
 import sys
 import telnetlib
 import time
+import logging
+import datetime
 
 port        = sys.argv[1]
 delay_on    = int(sys.argv[2])
@@ -14,6 +16,22 @@ password = 'apc'
 finish = '>'
 commands = ["whoami"]
 
+# log
+# logging
+formatter="%(asctime)s %(levelname)-12s %(message)s"
+
+# to file
+log_filename="log/log_"+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d.%H.%M.%S.%f')
+logging.basicConfig( filename=log_filename, filemode="a", format=formatter, level=logging.INFO);
+
+# to console
+formatter = logging.Formatter(formatter)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+# telnet
 tn = telnetlib.Telnet(Host, port=23, timeout=20)
 tn.set_debuglevel(0)
 
@@ -26,10 +44,12 @@ def do_telnet():
       
     tn.read_until(finish)
     while True:
+        logging.info('power on')
         tn.write('olOn ' + port + '\r')
         tn.read_until(finish)
         time.sleep(delay_on)
 
+        logging.info('power off')
         tn.write('olOff ' + port + '\r')
         tn.read_until(finish)
         time.sleep(delay_off)
@@ -39,7 +59,9 @@ def do_telnet():
 
 if __name__=='__main__':
     try:
+        logging.info('port=%s on=%d off=%d' % (port, delay_on, delay_off))
         do_telnet()
     except KeyboardInterrupt:
+        logging.info('ctrl-c')
         tn.write('%s\r' % 'olOff ' + port)
         tn.close() 

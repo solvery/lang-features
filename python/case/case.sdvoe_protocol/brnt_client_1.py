fecfdbf7f656a95c19c867e83a88fdd7bb89862b
solvery@163.com <solvery@163.com>
2017-10-18 16:32:38 +0800

    case.sdvoe_protocol

--- python/case/case.sdvoe_protocol/brnt_client_1.py
+++ python/case/case.sdvoe_protocol/brnt_client_1.py
@@ -1,14 +1,22 @@
-import socket,sys
+import socket
+import sys
+import struct
 
-addr=('localhost', 10000)
+bc_addr=('<broadcast>', 6969)
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.sendto("hello from client",addr)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 
-while 1:
-     data=s.recvfrom(1024)
-     if not data:
-        break
-     print data
+def hex2bin(data_hex):
+    data_bin=''
+    for d in data_hex:
+        data_bin = data_bin + struct.pack('B', d)
+    return data_bin
 
+
+data=hex2bin([0x07, 0x13, 0x03, 0x03, 0x01, 0x13, 0x97, 0x08, 0x01, 0x00] + [0x08, 0x00, 0x27, 0x65, 0xad, 0xf3] + [0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,0x00])
+s.sendto(data, bc_addr)
+data,addr=s.recvfrom(1024)
+print "got data from",addr,len(data)
+print " ".join(("%02x" % struct.unpack('B', n)) for n in data)
 

ec1c12f1557779aebaf9bf8006e921cf90c25697
solvery@163.com <solvery@163.com>
2017-10-18 19:09:19 +0800

    case.sdvoe_protocol

--- python/case/case.sdvoe_protocol/brnt_client_1.py
+++ python/case/case.sdvoe_protocol/brnt_client_1.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import struct
+import time
 
 bc_addr=('<broadcast>', 6969)
 
@@ -16,6 +17,11 @@ def hex2bin(data_hex):
 
 data=hex2bin([0x07, 0x13, 0x03, 0x03, 0x01, 0x13, 0x97, 0x08, 0x01, 0x00] + [0x08, 0x00, 0x27, 0x65, 0xad, 0xf3] + [0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,0x00])
 s.sendto(data, bc_addr)
+time.sleep(0.5)
+s.sendto(data, bc_addr)
+time.sleep(0.5)
+s.sendto(data, bc_addr)
+time.sleep(0.5)
 data,addr=s.recvfrom(1024)
 print "got data from",addr,len(data)
 print " ".join(("%02x" % struct.unpack('B', n)) for n in data)

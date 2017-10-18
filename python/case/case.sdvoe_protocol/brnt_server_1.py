99aef5a97e777f0c100d03501f8a2cf6ceebd9fc
solvery@163.com <solvery@163.com>
2017-10-18 10:29:11 +0800

    case.sdvoe_protocol

--- python/case/case.sdvoe_protocol/brnt_server_1.py
+++ python/case/case.sdvoe_protocol/brnt_server_1.py
@@ -1,13 +1,12 @@
 import socket
 import struct
+import time
 
 host=''
 port=6969
 
-sock_bc=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock_bc.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-sock_bc.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
-sock_bc.bind((host,port))
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind((host,port))
 
 def hex2bin(data_hex):
     data_bin=''
@@ -15,11 +14,24 @@ def hex2bin(data_hex):
         data_bin = data_bin + struct.pack('B', d)
     return data_bin
 
-while 1:
-    try:
-        data,addr=sock_bc.recvfrom(1024)
-        print "bc got data from",addr
-        print " ".join(("%02x" % struct.unpack('B', n)) for n in data)
+try:
+    data,addr=s.recvfrom(1024)
+    resp=hex2bin([0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0f, 0x00])
+    s.sendto(resp,('169.254.118.202', 6969))
+    print "got data from",addr,len(data)
+    print " ".join(("%02x" % struct.unpack('B', n)) for n in data)
+    print "send: ", " ".join(("%02x" % struct.unpack('B', n)) for n in resp)
+    time.sleep(0.2)
 
-    except KeyboardInterrupt:
-        raise
+    data,addr=s.recvfrom(1024)
+    resp=hex2bin([0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0x00])
+    s.sendto(resp,('169.254.118.202', 6969))
+    print "got data from",addr,len(data)
+    print " ".join(("%02x" % struct.unpack('B', n)) for n in data)
+    print "send: ", " ".join(("%02x" % struct.unpack('B', n)) for n in resp)
+
+    data,addr=s.recvfrom(1024)
+    print "got data from",addr,len(data)
+    print " ".join(("%02x" % struct.unpack('B', n)) for n in data)
+except KeyboardInterrupt:
+    raise

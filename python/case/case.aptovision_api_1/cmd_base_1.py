edfa31721b8d794dbbeedbb4778eb363bb459d97
solvery@163.com <solvery@163.com>
2017-11-01 19:20:28 +0800

    case.aptovision_api_1

--- python/case/case.aptovision_api_1/cmd_base_1.py
+++ python/case/case.aptovision_api_1/cmd_base_1.py
@@ -2,27 +2,22 @@ import sys
 import time
 import json
 
+import telnetlib
+#host='127.0.0.1'
+ip_addr = sys.argv[1]
+host=ip_addr
+port=6970
+tn = telnetlib.Telnet(host, port, timeout=20)
+tn.set_debuglevel(0)
+
 def nt_read(tn):
     s = tn.read_until('{')
     s += tn.read_very_eager()
     #print "read: \n" + s
     return s
     
-def do_telnet():
-    import telnetlib
-    #host='127.0.0.1'
-    ip_addr = sys.argv[1]
-    host=ip_addr
-    port=6970
-    tn = telnetlib.Telnet(host, port, timeout=20)
-    tn.set_debuglevel(0)
-
-    tn.write("mode human on" + '\n')
-    nt_read(tn)
-    tn.write("require blueriver_api 2.11.0" + '\n')
-    nt_read(tn)
-
-    tn.write("reboot ALL " + '\r')
+def cmd_send(cmd):
+    tn.write(cmd + ' \r')
     json_str = nt_read(tn)
     json_parsed = json.loads(json_str)
     request_id = str(json_parsed['request_id'])
@@ -30,9 +25,20 @@ def do_telnet():
     time.sleep(1)
     tn.write('request ' + request_id + '\r\n')
     json_str = nt_read(tn)
+    print cmd
     print json_str
-    tn.close() 
 
+def do_telnet():
+
+    tn.write("mode human on" + '\n')
+    nt_read(tn)
+    tn.write("require blueriver_api 2.11.0" + '\n')
+    nt_read(tn)
+
+    cmd = 'reboot ALL'
+    cmd_send(cmd)
+    tn.close() 
 
 if __name__=='__main__':
     do_telnet()
+

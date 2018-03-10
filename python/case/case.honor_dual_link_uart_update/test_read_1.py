4ad6ad7352fc1981e8e8132e63d71d0118765cda
solvery@163.com <solvery@163.com>
2018-03-10 20:13:52 +0800

    case.honor_dual_link_uart_update

--- python/case/case.honor_dual_link_uart_update/test_read_1.py
+++ python/case/case.honor_dual_link_uart_update/test_read_1.py
@@ -21,7 +21,6 @@ def uart_recv():
             else:
                 return '\b0'
 
-
 def print_hex(data):
     print (" ".join(("%02x" % n) for n in data))
 
@@ -32,6 +31,25 @@ def gen_sum(data):
     d1 = data_sum % 0x100
     d2 = data_sum / 0x100 % 0x100
     return [d1, d2]
+
+def cmd_get_version():
+    print sys._getframe().f_code.co_name
+    cmd = [0x1b, 0x3c, 0x55, 0x03]
+    cmd = cmd + gen_sum(cmd)
+    #print_hex(cmd)
+    return cmd
+
+
+def cmd_get_data(addr):
+    print sys._getframe().f_code.co_name
+
+    a1 = addr%0x100
+    a2 = addr/0x100%0x100
+    a3 = addr/0x10000%0x100
+    cmd = [0x1b, 0x3c, 0x55, 0x02, a1, a2, a3]
+    cmd = cmd + gen_sum(cmd)
+    #print_hex(cmd)
+    return cmd
 
 def cmd_flash_erase(addr):
     print sys._getframe().f_code.co_name
@@ -59,22 +77,6 @@ def cmd_flash_write(addr, data):
     #print_hex(cmd)
     return cmd
 
-def cmd_get_version():
-    print sys._getframe().f_code.co_name
-    cmd = [0x1b, 0x3c, 0x55, 0x03]
-    cmd = cmd + gen_sum(cmd)
-    #print_hex(cmd)
-    return cmd
-
-def bin2hex(data_bin):
-    data_hex_array = []
-    for d in data_bin:
-        data_hex = struct.unpack('B', d)
-        data_hex_array = data_hex_array + [data_hex[0]]
-    return data_hex_array
-
-
-
 def partition(lst, partition_size):
     if partition_size < 1:
         partition_size = 1
@@ -95,33 +97,40 @@ def get_file_data(fn):
     print "data append  len: 0x%x" % len(data)
     return data
 
+def bin2hex(data_bin):
+    data_hex_array = []
+    for d in data_bin:
+        data_hex = struct.unpack('B', d)
+        data_hex_array = data_hex_array + [data_hex[0]]
+    return data_hex_array
+
 def main():  
-    cmd = cmd_get_version()
+
+    #flash_start_addr = 0x10000
+    flash_start_addr = 0x400000
+    #cmd = cmd_get_version()
+    #ser.write(cmd)
+    #recv = uart_recv()
+    #recv_bytes = bin2hex(recv)
+    #print_hex(recv_bytes)
+    #time.sleep(0.5)  
+
+    for i in range(0x40):
+        cmd = cmd_get_data(flash_start_addr + 0x10000 * i)
+        ser.write(cmd)
+        recv = uart_recv()
+        recv_bytes = bin2hex(recv)
+        print_hex(recv_bytes)
+
+    i = 0
+    cmd  = cmd_flash_erase(flash_start_addr + 0x10000 * i)
+    #cmd  = cmd_flash_erase(0x400000 + 0x10000 * i)
     ser.write(cmd)
     recv = uart_recv()
     recv_bytes = bin2hex(recv)
     print_hex(recv_bytes)
-    time.sleep(0.5)  
 
-
-    #flash_start_addr = 0x0
-    #flash_start_addr = 0x10000
-    flash_start_addr = 0x400000
-    for i in range(0x40):
-        cmd  = cmd_flash_erase(flash_start_addr + (0x10000 * i))
-        ser.write(cmd)
-        print "processing %0.1f %%" % (100.0*i/0x100)
-        time.sleep(0.2)  
-
-    data_in = get_file_data("system_top.bin")
-    pkg_size = len(data_in)/0x100
-    for i in range(pkg_size):
-        data = data_in[i*0x100:(i+1)*0x100]
-        data_hex = [struct.unpack('B', n)[0] for n in data]
-        cmd  = cmd_flash_write(flash_start_addr + (0x100 * i), data_hex)
-        print "processing %0.1f %%" % (100.0*i/pkg_size)
-        ser.write(cmd)
-        #time.sleep(0.1)  
+    time.sleep(0.3)  
     exit()
     while True:  
         count = ser.inWaiting()  

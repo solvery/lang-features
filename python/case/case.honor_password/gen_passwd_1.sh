4b907bdcc4e15b955109fcd1209ffdaa323aaa66
solvery@163.com <solvery@163.com>
2018-03-14 16:39:14 +0800

    case.honor_password

---
+++ python/case/case.honor_password/gen_passwd_1.sh
@@ -0,0 +1,18 @@
+
+#control box
+python honor_password_1.py 810001+bios 
+python honor_password_1.py 810002+bios
+python honor_password_1.py 810003+bios
+
+python honor_password_1.py 810001+disk 
+python honor_password_1.py 810002+disk
+python honor_password_1.py 810003+disk
+
+python honor_password_1.py 810001+root 
+python honor_password_1.py 810002+root
+python honor_password_1.py 810003+root
+
+# switch
+python honor_password_1.py 910001+admin 
+python honor_password_1.py 910002+admin
+python honor_password_1.py 910003+admin

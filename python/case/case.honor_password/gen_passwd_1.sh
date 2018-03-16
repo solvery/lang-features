5be4c5fd86e7d281264a01cc9f6423cf043e636f
solvery@163.com <solvery@163.com>
2018-03-16 14:52:43 +0800

    case.aptovision_api_1

--- python/case/case.honor_password/gen_passwd_1.sh
+++ python/case/case.honor_password/gen_passwd_1.sh
@@ -4,10 +4,6 @@ python honor_password_1.py 810001+bios
 python honor_password_1.py 810002+bios
 python honor_password_1.py 810003+bios
 
-python honor_password_1.py 810001+disk 
-python honor_password_1.py 810002+disk
-python honor_password_1.py 810003+disk
-
 python honor_password_1.py 810001+root 
 python honor_password_1.py 810002+root
 python honor_password_1.py 810003+root
